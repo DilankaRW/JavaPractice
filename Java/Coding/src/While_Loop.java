0f3c7853ec5c8096280157981bd5779a6cb31c7b
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-08 03:05:14 +0530

    While Loop

---
+++ Java/Coding/src/While_Loop.java
@@ -0,0 +1,17 @@
+public class While_Loop {
+    public static void main(String[] args) {
+//        int i = 1;
+//        while (i<=10){
+//            System.out.println("Hello");
+//            i++;  // i = i + 1
+//        }
+          /* Output - Hello is displayed ten times (Hello,Hello,Hello,Hello...) */
+
+        int i = 10;
+        while (i<=20){
+            System.out.println(i);
+            i = i+2;
+        }
+        /* Output - 10, 12, 14, 16, 18, 20 */
+    }
+}

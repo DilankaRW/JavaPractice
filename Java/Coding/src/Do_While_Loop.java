114eab25acdf6892911ac375971da039809b701e
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-08 03:23:59 +0530

    Do While Loop

---
+++ Java/Coding/src/Do_While_Loop.java
@@ -0,0 +1,9 @@
+public class Do_While_Loop {
+    public static void main(String[] args) {
+        int i = 15;
+        do {
+            System.out.println("Hello");
+            i++;
+        }while (i<=10);
+    }
+}

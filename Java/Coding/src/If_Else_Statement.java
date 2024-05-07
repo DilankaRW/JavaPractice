a5014c65ae492dc7664c04d91e1c6d0bb0229681
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-07 23:48:44 +0530

    if else statement

---
+++ Java/Coding/src/If_Else_Statement.java
@@ -0,0 +1,16 @@
+// Equal Operator  ==
+// Not Equal Operator  !=
+// Greater than Operator   >
+// Less than Operator   <
+// Greater than or Equal   >=
+// Less than or Equal   <=
+public class If_Else_Statement {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x == 10){
+            System.out.println("Correct");
+        }else{
+            System.out.println("Wrong");
+        }
+    }
+}

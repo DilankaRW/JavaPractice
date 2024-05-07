f4f1827bd43a41e6a15cab3d43da22c9961ad72f
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-08 00:08:56 +0530

    Logical Operators

--- Java/Coding/src/Logical_Operators.java
+++ Java/Coding/src/Logical_Operators.java
@@ -1,8 +1,29 @@
 //Logical Operators
-//1. AND
-//2. OR
+//1. AND - &&
+//2. OR - ||
+
 public class Logical_Operators {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 5;
+        int y = 15;
+
+//        AND Operator - &&
+
+         if (x>10 && y>10){
+             System.out.println("Positive");
+         }else{
+             System.out.println("Negative");
+         }
+         // Output is Negative
+
+
+//        OR Operators - ||
+
+        if (x>10 || y>10){
+            System.out.println("Positive");
+        }else{
+            System.out.println("Negative");
+        }
+        // Output is Positive
     }
 }

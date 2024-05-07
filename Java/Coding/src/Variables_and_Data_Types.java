cc5d2fe261cb0858a1632cdd2dc13280ec9265c0
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-07 16:51:53 +0530

    Variables and Data Types

---
+++ Java/Coding/src/Variables_and_Data_Types.java
@@ -0,0 +1,18 @@
+public class Variables_and_Data_Types {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+
+        double b = 10.25;
+        System.out.println(b);
+
+        char c = 'C';
+        System.out.println(c);
+
+        boolean d = true;
+        System.out.println(d);
+
+        String e = "Hello!";
+        System.out.println(e);
+    }
+}

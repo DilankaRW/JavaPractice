3e3694eb07c6245a810ae7da7c4320180cb64215
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-07 23:36:25 +0530

    Assignment Operators

---
+++ Java/Coding/src/Assignment_Operators.java
@@ -0,0 +1,39 @@
+public class Assignment_Operators {
+    public static void main(String[] args) {
+
+//        int x = 10;
+//        x++; // x = x + 1;
+//        System.out.println(x);
+
+//        int x = 10;
+//        int y = x++;
+//        System.out.println(x); /* answer is = 11 */
+//        System.out.println(y); /* answer is = 10 */
+
+//        int x = 10;
+//        int y = ++x;
+//        System.out.println(x); /* answer is = 11 */
+//        System.out.println(y); /* answer is = 11 */
+
+//        int x = 10;
+//        x--; // x = x-1
+//        System.out.println(x); /* answer is = 9 */
+
+//        int x = 10;
+//        x = x+5;
+//        x += 5;
+//        System.out.println(x); /* answer is = 15 */
+
+//        int x = 10;
+//        x -= 5;
+//        System.out.println(x); /* answer is = 5 */
+
+//        int x = 10;
+//        x *= 5;
+//        System.out.println(x); /* answer is = 50 */
+
+        int x = 10;
+        x /= 5; // x = x/5;
+        System.out.println(x); /* answer is = 2 */
+    }
+}

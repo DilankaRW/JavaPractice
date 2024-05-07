c7db33528acb5763ecfb12ecdcc16b394f51cb6d
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-07 23:09:38 +0530

    Mathematical Operators

---
+++ Java/Coding/src/Mathematical_Operators.java
@@ -0,0 +1,38 @@
+//Mathematical Operators
+
+//Addition (+)
+//Subtraction (-)
+//Multiplication (*)
+//Division (/)
+//Modulus (%)
+public class Mathematical_Operators {
+    public static void main(String[] args) {
+//        int x = 10;
+//        int y = 5;
+
+        int y,x,z,answer;
+        x = 10;
+        y = 5;
+        z = 4;
+        answer = x+y;
+        System.out.println("Answer = " + answer);
+
+        answer = x-y;
+        System.out.println("Answer = " + answer);
+
+        answer = x*y;
+        System.out.println("Answer = " + answer);
+
+        answer = x/y;
+        System.out.println("Answer = " + answer);
+
+        double a,b,answer1;
+        a = 10;
+        b = 4;
+        answer1 = a/b;
+        System.out.println("Answer = " + answer1);
+
+        answer = x%z;
+        System.out.println("Answer = " + answer);
+    }
+}

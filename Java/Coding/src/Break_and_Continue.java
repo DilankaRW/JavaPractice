16f8ec2bb0acc1e904a3331baf5cada20d50a904
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-08 12:42:17 +0530

    Break and Continue statements

---
+++ Java/Coding/src/Break_and_Continue.java
@@ -0,0 +1,25 @@
+public class Break_and_Continue {
+    public static void main(String[] args) {
+
+        /* Break Statement */
+
+//        for (int i=1; i<=10; i++){
+//            if (i==5)
+//                break;
+//            System.out.println(i);
+//        }
+        /* Output - 1,2,3,4 */
+
+
+        /* Continue Statement */
+
+        for (int i=1; i<=10; i++){
+            if (i==5)
+                continue;
+            System.out.println(i);
+        }
+        /* Output - 1,2,3,4,6,7,8,9,10 */
+        /* Number 5 does not print */
+
+    }
+}

65e9a53af9cba64db31c50a4fef49674331c6597
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-12 19:03:07 +0530

    Final Keyword

---
+++ Java/Coding/src/Final_Keyword.java
@@ -0,0 +1,34 @@
+/************ final for Variable ************/
+//public class Final_Keyword {
+//    public static void main(String[] args) {
+//        final double  g = 9.81;
+////        g = 45; // Cannot assign a value to final variable 'g'
+//        System.out.println(g);
+//    }
+//}
+
+
+/************ final for Method ************/
+//class Parent{
+//    final public void say(){
+//        System.out.println("Hiii");
+//    }
+//}
+//class Child extends Parent{
+//    public void say(){ //'say()' cannot override 'say()' in 'Parent'; overridden method is final
+//        System.out.println("Hello");
+//    }
+//}
+//public class Final_Keyword {
+//    public static void main(String[] args) {
+//        Child obj = new Child();
+//        obj.say();
+//    }
+//}
+
+/************ final for Class ************/
+final class Parent{
+}
+class Child extends Parent{ // Cannot inherit from final 'Parent'
+}
+

d79e29d2a9c568f92dc65a125a0141a5d5e97063
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-13 23:57:00 +0530

    Multiple Inheritance using Interface

---
+++ Java/Coding/src/Inheritance_Multiple.java
@@ -0,0 +1,28 @@
+/* Multiple Inheritance using Interface */
+/*
+class A {
+}
+interface B{
+}
+class C extends A implements B{
+}
+*/
+
+
+/*
+interface A {
+}
+interface B{
+}
+class C implements A,B{
+}
+*/
+
+
+
+interface A {
+}
+interface B{
+}
+interface C extends A,B{
+}

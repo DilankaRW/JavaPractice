300d5a4b87856bd4107eb26637f866aec3cee6f5
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-14 22:07:41 +0530

    Super keyword

--- Java/Coding/src/Super_keyword.java
+++ Java/Coding/src/Super_keyword.java
@@ -1,5 +1,68 @@
+/* Super keyword for Variables */
+//class AA{
+//    int x =10;
+//}
+//
+//class BB extends AA{
+//    int x = 15;
+//    public void print(){
+//        System.out.println(x);
+//        /* Output - 15 */
+//        System.out.println(super.x); // Super keyword
+//        /* Output - 10 */
+//    }
+//}
+//public class Super_keyword {
+//    public static void main(String[] args) {
+//        BB obj = new BB();
+//        obj.print();
+//    }
+//}
+
+
+/* Super keyword for methods */
+//class AA{
+//    void show(){
+//        System.out.println("Hi");
+//    }
+//}
+//
+//class BB extends AA{
+//    void show(){
+//        super.show(); // Super keyword
+//        System.out.println("Hello");
+//        /* Output - Hi, Hello */
+//    }
+//}
+//public class Super_keyword {
+//    public static void main(String[] args) {
+//        BB obj = new BB();
+//        obj.show();
+//    }
+//}
+
+
+/* Super keyword for constructors */
+class Student{
+    int age;
+    String school;
+    String email;
+    Student(int age, String school, String email){
+        this.age = age;
+        this.school = school;
+        this.email = email;
+    }
+}
+
+class Kamal extends Student{
+    Kamal(int age, String school, String email){
+        super(age, school, email); // Super keyword
+    }
+}
+
 public class Super_keyword {
     public static void main(String[] args) {
-        
+        Kamal obj = new Kamal(15,"ABCD","hello@email");
+        System.out.println(obj.school);
     }
 }

4e9b146e29208d0b63f2ade2dabab04166e1669a
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-15 14:25:43 +0530

    Object Class

---
+++ Java/Coding/src/Object_Class.java
@@ -0,0 +1,20 @@
+class Employee extends Object{
+    String name = "Kamal";
+    int age = 15;
+    void run(){
+    }
+    void eat(){
+    }
+}
+public class Object_Class extends Employee{
+    public static void main(String[] args) {
+
+        Object o1 =new Object(); // Object from Object class (java default class (Parent class of all classes))
+        System.out.println(o1.getClass());
+        /* Output - class java.lang.Object */
+
+        Employee obj = new Employee();
+        System.out.println(obj.getClass());
+        /* Output - class Employee */
+    }
+}

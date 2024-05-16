02d289527907c8bba996304651db04178c996b98
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-16 23:50:50 +0530

    Generics

--- Java/Coding/src/Generics.java
+++ Java/Coding/src/Generics.java
@@ -1,13 +1,26 @@
-class Wow<T>{ // Generics type class - This is a generic class declaration where T is a type parameter. It means that Wow can be instantiated with any reference type.
-    void print(T x){
-        T y = (T) x;
-        System.out.println(y);
-    }
-}
-public class Generics {
+//class Wow<T>{ // Generics type class - This is a generic class declaration where T is a type parameter. It means that Wow can be instantiated with any reference type.
+//    void print(T x){
+//        T y = (T) x;
+//        System.out.println(y);
+//    }
+//}
+//public class Generics {
+//    public static void main(String[] args) {
+//        Wow<Integer> obj = new Wow();
+//        obj.print(34);
+//        /* Output - 34 */
+//    }
+//}
+
+
+
+public class Generics <T>{
+    T[] arr = (T[]) new Object[3]; // // Generics type array
+
     public static void main(String[] args) {
-        Wow<Integer> obj = new Wow();
-        obj.print(34);
-        /* Output - 34 */
+        Generics obj = new Generics();
+        obj.arr[0] = 32;
+        obj.arr[1] = "Hello";
+        System.out.println(obj.arr[1]);
     }
 }

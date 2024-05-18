8842e7e38216391086ce234335a76a2b5e42b195
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-19 00:08:48 +0530

    Read file Using Scanner class

--- Java/Coding/src/File_Handling/ReadFile1.java
+++ Java/Coding/src/File_Handling/ReadFile1.java
@@ -1,3 +1,5 @@
+/* Using Scanner class */
+
 package File_Handling;
 
 import java.io.File;
@@ -5,9 +7,13 @@ import java.util.Scanner;
 
 public class ReadFile1 {
     public static void main(String[] args) {
-        File file = new File("file.txt");
+        File file = new File("src/File_Handling/file.txt");
         try {
             Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()){
+                System.out.println(reader.nextLine());
+            }
+            reader.close();
         }catch (Exception e){
             System.out.println(e);
         }

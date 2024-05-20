b7ea4689a9ba0bdcd01f33c0ca9cac9ed8153567
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-20 13:31:34 +0530

    Start project 1 backend

---
+++ Spring Boot/Project 1/backend/src/test/java/com/practiceproject/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiceproject.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

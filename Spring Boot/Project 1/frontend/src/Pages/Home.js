12ecb3a226a87a029bbddc9335c0b43f06535fe4
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-23 13:58:54 +0530

    Display Information using AXIOS GET - REACT

--- Spring Boot/Project 1/frontend/src/Pages/Home.js
+++ Spring Boot/Project 1/frontend/src/Pages/Home.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-    const [users, setUsers] = useState([]);
 
-    useEffect(() => {
+    const [Users, setUsers] =  useState([]);
+
+
+    useEffect(() =>{
         loadUsers();
-    }, []);
+    });
 
-    const loadUsers = async () => {
+    const loadUsers = async() =>{
         const result = await axios.get("http://localhost:8080/users");
         setUsers(result.data);
     };
+
+
 
   return (
     <div className='container'>
@@ -20,24 +25,21 @@ export default function Home() {
         <thead>
             <tr>
             <th scope="col">ID</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
+            <th scope="col">Name</th>
+            <th scope="col">Userame</th>
             <th scope="col">Email</th>
             <th scope="col">Action</th>
             </tr>
         </thead>
         <tbody>
             {
-                users.map((user,index)=>(
+                Users.map((user,index)=>(
                     <tr>
                     <th scope="row" key={index}>{index+1}</th>
                     <td>{user.name}</td>
                     <td>{user.username}</td>
                     <td>{user.email}</td>
                     <td>
-                        <Link className='btn btn-primary max-2' to={`/viewemployee/${employee.id}`}>View</Link>
-                        <Link className='btn btn-outline-primary max-2' to={`/editemployee/${employee.id}`}>Edit</Link>
-                        <button className='btn btn-danger max-2' onClick={() => deleteEmployee(employee.id)}>Delete</button>
                     </td>
                     </tr>
                 ))
--- Spring Boot/Project 1/frontend/src/layout/Navbar.js
+++ Spring Boot/Project 1/frontend/src/layout/Navbar.js
@@ -5,11 +5,11 @@ export default function Navbar() {
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">User Data Management System</Link>
+                <a className="navbar-brand" to="/">User Data Management System</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <Link className='btn btn-light' to="/addemployee">Add User</Link>
+                <a className='btn btn-light' to="/addemployee">Add User</a>
             </div>
         </nav>
     </div>

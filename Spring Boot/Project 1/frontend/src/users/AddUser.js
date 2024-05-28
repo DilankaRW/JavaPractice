2540617320efcbc55ee1f674a1fc90d4c8003ed9
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-28 20:03:22 +0530

    Post data into database AXIOS POST

--- Spring Boot/Project 1/frontend/src/users/AddUser.js
+++ Spring Boot/Project 1/frontend/src/users/AddUser.js
@@ -1,6 +1,10 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function AddUser() {
+
+  let navigate = useNavigate()
 
   const [user, setUser]=useState({
     name:"",
@@ -14,11 +18,19 @@ export default function AddUser() {
     setUser({...user, [e.target.name]: e.target.value});
   };
 
+  const onSubmit = async (e)=>{
+    e.preventDefault();
+    await axios.post("http://localhost:8080/user",user)
+    navigate("/");
+  };
+
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
           <h2 className='text-center m-4'>Add New User</h2>
+
+          <form onSubmit={(e) => onSubmit(e)}>
           <div className='mb-3'>
             <label htmlFor="Name" className='form-label'>Name</label>
             <input type='text' className='form-control' placeholder='Enter your Name' name='name' value={name} onChange={(e)=>onInputChange(e)}/>
@@ -33,6 +45,8 @@ export default function AddUser() {
           </div>
           <button type='submit' className='btn btn-outline-primary'>Submit</button>
           <button type='submit' className='btn btn-outline-danger mx-2'>Cancel</button>
+          </form>
+
         </div>
       </div>
     </div>

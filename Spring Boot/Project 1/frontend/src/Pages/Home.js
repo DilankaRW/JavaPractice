28c139e16d4bce2719b3b3c3ab4e0b83ca09252a
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-01 21:52:55 +0530

    Edit user using AXIOS PUT - REACT

--- Spring Boot/Project 1/frontend/src/Pages/Home.js
+++ Spring Boot/Project 1/frontend/src/Pages/Home.js
@@ -41,7 +41,7 @@ export default function Home() {
                     <td>{user.email}</td>
                     <td>
                     <button className='btn btn-primary mx-2'>View</button>
-                    <button className='btn btn-outline-primary mx-2'>Edit</button>
+                    <Link className='btn btn-outline-primary mx-2' to={`/edituser/${user.id}`}>Edit</Link>
                     <button className='btn btn-danger mx-2'>Delete</button>
                     </td>
                     </tr>

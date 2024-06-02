502d8129a0d201e7b1b33871e0bfbe5fe50f89fd
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-02 23:22:46 +0530

    View Information of specific User

--- Spring Boot/Project 1/frontend/src/App.js
+++ Spring Boot/Project 1/frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home';
 import AddUser from './users/AddUser';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EditUser from './users/EditUser';
+import ViewUser from './users/ViewUser';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Navbar/>
       <Routes>
       <Route exact path='/' element={<Home/>} />
+      <Route exact path='/viewuser/:id' element={<ViewUser/>} />
       <Route exact path='/adduser' element={<AddUser/>} />
       <Route exact path='/edituser/:id' element={<EditUser/>} />
       </Routes>

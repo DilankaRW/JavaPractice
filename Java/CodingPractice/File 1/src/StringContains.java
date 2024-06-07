e88e71821ce00aa48fa949ba731af91d7d22592b
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-08 00:06:53 +0530

    program to check if a vowel is present in a string.

--- Java/CodingPractice/File 1/src/StringContains.java
+++ Java/CodingPractice/File 1/src/StringContains.java
@@ -1,12 +1,13 @@
 // Program to check if a vowel is present in a string.
-public class StringContains {
-    public static void main(String[] args) {
-        System.out.println(stringContainsVowels("Hello"));
-        System.out.println(stringContainsVowels("TV"));
-    }
 
+public class StringContains {
     public static boolean stringContainsVowels(String input) {
         return input.toLowerCase().matches(".*[aeiou].*");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(stringContainsVowels("Hello")); // true
+        System.out.println(stringContainsVowels("TV"));  // false
     }
 }
 

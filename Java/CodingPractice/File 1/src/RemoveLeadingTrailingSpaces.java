a7de926e0080bf82f8824a65dcf09149e11f3b99
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-09 11:19:03 +0530

    Remove Leading & Trailing Spaces.

--- Java/CodingPractice/File 1/src/RemoveLeadingTrailingSpaces.java
+++ Java/CodingPractice/File 1/src/RemoveLeadingTrailingSpaces.java
@@ -1,12 +1,17 @@
-// remove leading and trailing spaces from a string
+// Remove leading and trailing spaces from a string.
 public class RemoveLeadingTrailingSpaces {
     public static void main(String[] args) {
         String str = "  Hello World!  ";
         String trimmedStr = str.trim();
-        System.out.println("Original String: '"+ str +"'");
         System.out.println("Trimmed String: '"+ trimmedStr +"'");
+        // Trimmed String: 'Hello World!'
 
-        //Original String: '  Hello World!  '
-        //Trimmed String: 'Hello World!'
+        // Remove leading spaces from a string
+        String trimmedStr1 = str.replaceAll("^\\s+", "");
+        System.out.println("String without leading spaces: '" + trimmedStr1 + "'");
+
+        // Remove trailing spaces from a string
+        String trimmedStr2 = str.replaceAll("\\s+$", "");
+        System.out.println("String without trailing spaces: '" + trimmedStr2 + "'");
     }
 }

0d13c263680df664af4b0d1fe8d987bd82e963d2
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-09 23:30:10 +0530

    Factorial Of Integer

---
+++ Java/CodingPractice/File 1/src/FactorialOfInteger.java
@@ -0,0 +1,12 @@
+// Find the factorial of an integer.
+public class FactorialOfInteger {
+    public static void main(String[] args) {
+        int number = 5; // example number
+        long factorial = 1;
+
+        for (int i = 1; i <= number; i++){
+            factorial *= i;
+        }
+        System.out.println("Factorial of " + number + " is " + factorial);
+    }
+}

c5c896d8b687714fdd5b9a788e2a895bd4254b55
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-10 23:59:30 +0530

    Find String In File

--- Java/CodingPractice/File 1/src/FindStringInFile.java
+++ Java/CodingPractice/File 1/src/FindStringInFile.java
@@ -1,25 +1,55 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+// Find a string in a text file
+import java.io.*;
 import java.util.Scanner;
 
 // Find a string in a text file
 public class FindStringInFile {
 
-    // The following example code shows how to use the Scanner class to read the file contents line by line and then use the String contains() method to check if the string is present in the file:
-    boolean findStringInFile(String filePath, String str) throws FileNotFoundException {
-        File file = new File(filePath);
+    public static void main(String[] args) {
+        // Specify the file path and the string to search for
+        String filePath = "path/to/your/file.txt";
+        String searchString = "your search string";
 
-        Scanner scanner = new Scanner(file);
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int lineNumber = 0;
+            boolean found = false;
 
-        // read the file line by line
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (line.contains(str)) {
-                scanner.close();
-                return true;
+            // Read file line by line
+            while ((line = br.readLine()) != null) {
+                lineNumber++;
+                // Check if the line contains the search string
+                if (line.contains(searchString)) {
+                    System.out.println("String found at line " + lineNumber + ": " + line);
+                    found = true;
+                    // Optionally, break if you only want the first occurrence
+                    // break;
+                }
             }
+
+            if (!found) {
+                System.out.println("String not found in the file.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        scanner.close();
-        return false;
     }
+
+    // The following example code shows how to use the Scanner class to read the file contents line by line and then use the String contains() method to check if the string is present in the file:
+//    boolean findStringInFile(String filePath, String str) throws FileNotFoundException {
+//        File file = new File(filePath);
+//
+//        Scanner scanner = new Scanner(file);
+//
+//        // read the file line by line
+//        while (scanner.hasNextLine()) {
+//            String line = scanner.nextLine();
+//            if (line.contains(str)) {
+//                scanner.close();
+//                return true;
+//            }
+//        }
+//        scanner.close();
+//        return false;
+//    }
 }

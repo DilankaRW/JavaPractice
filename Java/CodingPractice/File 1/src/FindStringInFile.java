5608e1903c48485cb7e64cf9e346fe77426a0fce
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-10 23:58:07 +0530

    Find String In File

---
+++ Java/CodingPractice/File 1/src/FindStringInFile.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+// Find a string in a text file
+public class FindStringInFile {
+
+    // The following example code shows how to use the Scanner class to read the file contents line by line and then use the String contains() method to check if the string is present in the file:
+    boolean findStringInFile(String filePath, String str) throws FileNotFoundException {
+        File file = new File(filePath);
+
+        Scanner scanner = new Scanner(file);
+
+        // read the file line by line
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.contains(str)) {
+                scanner.close();
+                return true;
+            }
+        }
+        scanner.close();
+        return false;
+    }
+}

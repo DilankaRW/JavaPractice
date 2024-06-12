3d1449666c600a81726a68c2166748d14e284820
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-13 02:54:40 +0530

    Text Block

---
+++ Java/CodingPractice/File 1/src/TextBlock.java
@@ -0,0 +1,11 @@
+// Create text blocks in Java.
+public class TextBlock {
+    public static void main(String[] args) {
+        String TextBlock = """
+                This is a text block.
+                It can span multiple lines.
+                Each new line in the source code is also a new line in the string.
+                """;
+        System.out.println(TextBlock);
+    }
+}

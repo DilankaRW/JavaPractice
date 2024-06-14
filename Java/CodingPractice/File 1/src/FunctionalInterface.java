d9b704546514739662e9cba1485dc2320083a467
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-14 22:14:49 +0530

    Functional Interface

---
+++ Java/CodingPractice/File 1/src/FunctionalInterface.java
@@ -0,0 +1,7 @@
+// Create a functional interface
+// An interface with exactly one abstract method is called a functional interface. The major benefit of functional interfaces is that you can use lambda expressions to instantiate them and avoid using bulky anonymous class implementation.
+
+@java.lang.FunctionalInterface
+interface FunctionalInterface {
+    void test();
+}

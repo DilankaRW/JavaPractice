56dfbcb31e1f68ede53016bd52d82912f1c2346f
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-14 22:17:40 +0530

    Lambda Expressions

---
+++ Java/CodingPractice/File 1/src/LambdaExpressions.java
@@ -0,0 +1,5 @@
+// Show an example of using lambda expressions in Java.
+// Runnable is an excellent example of a functional interface. You can use lambda expressions to create a runnable.
+public class LambdaExpressions {
+    Runnable r1 = () -> System.out.println("My Runnable");
+}

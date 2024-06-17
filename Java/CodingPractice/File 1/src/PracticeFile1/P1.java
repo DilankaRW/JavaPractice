7f96b379c5a6bc0a170756420a661fd40fe05b8c
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-17 19:14:09 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P1.java
@@ -0,0 +1,9 @@
+package PracticeFile1;
+
+public class P1 {
+    public static void main(String[] args) {
+        String O = "Testing";
+        String R = new StringBuilder(O).reverse().toString();
+        System.out.println(R); // gnitseT
+    }
+}

3406fd3e18207a4e0bb102de35d2bd18cf66bafc
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-17 19:28:00 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P6.java
@@ -0,0 +1,10 @@
+// Remove Character
+package PracticeFile1;
+
+public class P6 {
+    public static void main(String[] args) {
+        String O = "fSfrfiff Lfaffnfkffafffff";
+        O = O.replaceAll("f","");
+        System.out.println(O); // Sri Lanka
+    }
+}

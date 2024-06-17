6af11ba65cc71e12e9328805264a4110cd0bb504
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-17 19:22:37 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P5.java
@@ -0,0 +1,10 @@
+// String Reverse
+package PracticeFile1;
+
+public class P5 {
+    public static void main(String[] args) {
+        String O = "Colombo";
+        String R = new StringBuilder(O).reverse().toString();
+        System.out.println(R); // obmoloC
+    }
+}

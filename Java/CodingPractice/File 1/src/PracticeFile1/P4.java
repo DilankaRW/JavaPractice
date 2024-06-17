a3658e6e81b956f088d29bd3a5140a179978914d
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-17 19:19:44 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P4.java
@@ -0,0 +1,10 @@
+// String Reverse
+package PracticeFile1;
+
+public class P4 {
+    public static void main(String[] args) {
+        String O = "Sri Lanka";
+        String R = new StringBuilder(O).reverse().toString();
+        System.out.println(R); // aknaL irS
+    }
+}

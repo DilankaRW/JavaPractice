d55208eb4839f19c0d55c5397e747e55d78378ce
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-17 19:34:41 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P10.java
@@ -0,0 +1,10 @@
+// Remove Character
+package PracticeFile1;
+
+public class P10 {
+    public static void main(String[] args) {
+        String O = "Diggglgaggngka";
+        O = O.replaceAll("g","");
+        System.out.println(O); // Dilanka
+    }
+}

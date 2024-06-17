fecd9193778bbfe9f768e8f37670c6f3bc255d30
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-17 19:31:43 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P8.java
@@ -0,0 +1,10 @@
+// Remove Character
+package PracticeFile1;
+
+public class P8 {
+    public static void main(String[] args) {
+        String O = "Hyyelylyyyo";
+        O = O.replaceAll("y","");
+        System.out.println(O); // Hello
+    }
+}

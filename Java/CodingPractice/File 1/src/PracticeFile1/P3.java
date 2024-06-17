6355b695bc5afe4e9eb027797ad5cc3ea59ec9f8
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-17 19:17:45 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P3.java
@@ -0,0 +1,10 @@
+// String Reverse
+package PracticeFile1;
+
+public class P3 {
+    public static void main(String[] args) {
+        String O = "Reverse";
+        String R = new StringBuilder(O).reverse().toString();
+        System.out.println(R); // esreveR
+    }
+}

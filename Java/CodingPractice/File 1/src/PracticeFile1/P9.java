9b237dc557dc4b2ac68c04f42e7222c891889698
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-17 19:33:21 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P9.java
@@ -0,0 +1,10 @@
+// Remove Character
+package PracticeFile1;
+
+public class P9 {
+    public static void main(String[] args) {
+        String O = "Shhuphher";
+        O = O.replaceAll("h","");
+        System.out.println(O); // Super
+    }
+}

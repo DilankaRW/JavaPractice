cc048acf3b51eac3e6c3a7b831613a0b969e045e
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-17 19:29:40 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P7.java
@@ -0,0 +1,10 @@
+// Remove Character
+package PracticeFile1;
+
+public class P7 {
+    public static void main(String[] args) {
+        String O = "dCdolddodddmbdod";
+        O = O.replaceAll("d","");
+        System.out.println(O); // Colombo
+    }
+}

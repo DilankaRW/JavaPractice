fb958a6601c2e46a22c9157a7f033cc35f8f7bd0
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-18 23:15:58 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P14.java
@@ -0,0 +1,10 @@
+// Remove leading and trailing spaces from a string.
+package PracticeFile1;
+
+public class P14 {
+    public static void main(String[] args) {
+        String S = "  DilankaRW  ";
+        String TS = S.trim();
+        System.out.println(TS); // DilankaRW
+    }
+}
---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P15.java
@@ -0,0 +1,10 @@
+// Remove leading and trailing spaces from a string.
+package PracticeFile1;
+
+public class P15 {
+    public static void main(String[] args) {
+        String S = "   Hello Sri Lanka  ";
+        String TS = S.trim();
+        System.out.println(TS); // Hello Sri Lanka
+    }
+}

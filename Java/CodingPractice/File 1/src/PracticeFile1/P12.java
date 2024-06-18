4ff86a4fb9a967fdee9e814fc0d3205fb4f128c4
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-18 23:06:28 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P12.java
@@ -0,0 +1,10 @@
+// Remove leading and trailing spaces from a string.
+package PracticeFile1;
+
+public class P12 {
+    public static void main(String[] args) {
+        String S = "  Colombo  ";
+        String TS = S.trim();
+        System.out.println(TS); // Colombo
+    }
+}

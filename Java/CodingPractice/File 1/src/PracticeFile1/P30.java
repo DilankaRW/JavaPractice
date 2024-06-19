0564f74af1f193c98f8eb7503c82e1bf0bbfa5b8
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-19 23:42:25 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P30.java
@@ -0,0 +1,12 @@
+// Sort Array
+package PracticeFile1;
+
+import java.util.Arrays;
+
+public class P30 {
+    public static void main(String[] args) {
+        int[] num = {54,84,65,8,4,54,58,545,2335,86,655,};
+        Arrays.sort(num);
+        System.out.println(Arrays.toString(num)); // [4, 8, 54, 54, 58, 65, 84, 86, 545, 655, 2335]
+    }
+}

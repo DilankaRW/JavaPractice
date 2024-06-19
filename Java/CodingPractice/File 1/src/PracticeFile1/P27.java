6be7b6436ecfe83d02e700e4e6231994ba0b564e
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-19 23:32:21 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile1/P27.java
@@ -0,0 +1,12 @@
+// Sort Array
+package PracticeFile1;
+
+import java.util.Arrays;
+
+public class P27 {
+    public static void main(String[] args) {
+        String[] names = {"sdra","Iosd","Xeds"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names)); // [Iosd, Xeds, sdra]
+    }
+}

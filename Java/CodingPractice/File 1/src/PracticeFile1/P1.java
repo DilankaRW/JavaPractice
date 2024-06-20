38927f5f3d9a9c2da59b2cbb4d6646179976b892
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-20 23:59:47 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P1.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P1.java
@@ -1,10 +1,32 @@
 // Remove Character
 package PracticeFile1;
 
+import java.util.Arrays;
+
 public class P1 {
     public static void main(String[] args) {
-        String O = "Tdedddstdidndddg";
-        O = O.replaceAll("d","");
-        System.out.println(O); // Testing
+        String S = "aknaL irS";
+        String RS = new StringBuilder(S).reverse().toString();
+        System.out.println(RS); // Sri Lanka
+
+        String O = "Ciiiiiiiiioiliioiiimiboiiiiii";
+        O = O.replaceAll("i","");
+        System.out.println(O); // Colombo
+
+        String RLTS = "       Hello       ";
+        String TS = RLTS.trim();
+        System.out.println(TS); // Hello
+
+        String RLS = "    Hello";
+        RLS = RLS.replaceAll("^\\s+","");
+        System.out.println(RLS); // Hello
+
+        String RTS = "Hello          ";
+        RTS = RTS.replaceAll("\\s+$","");
+        System.out.println(RTS); // Hello
+
+        String[] names = {"sdgff","dstys","fdds"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names)); // [dstys, fdds, sdgff]
     }
 }

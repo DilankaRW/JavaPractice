226617328c4e9abf2fa40685d0624929ee11a3d5
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-22 11:19:52 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P8.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P8.java
@@ -1,10 +1,42 @@
 // String Reverce
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+
 package PracticeFile1;
+
+import java.util.Arrays;
 
 public class P8 {
     public static void main(String[] args) {
-        String O = "Hello";
-        String RS = new StringBuilder(O).reverse().toString();
-        System.out.println(RS); // olleH
+        String S1 = "Hello";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "Helggggggggglo";
+        S2 = S2.replaceAll("g","");
+        System.out.println(S2);
+
+        String S3 = "             Hello     ";
+        S3 = S3.trim();
+        System.out.println(S3);
+
+        String S4 = "       Hello";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
+
+        String S5 = "Hello          ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
+        String[] names = {"i","H","G","S","O","L"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
+
+        Integer[] num = {9,8,7,6,5,4,3,2,1,0};
+        Arrays.sort(num);
+        System.out.println(Arrays.toString(num));
     }
 }

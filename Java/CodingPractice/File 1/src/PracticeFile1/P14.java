08b8f0b2696b3c25fcc352013e4fa88f73554a6c
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-23 23:53:52 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P14.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P14.java
@@ -1,10 +1,42 @@
+// String Reverse
+// Remove Character
 // Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+
 package PracticeFile1;
+
+import java.util.Arrays;
 
 public class P14 {
     public static void main(String[] args) {
-        String S = "  DilankaRW  ";
-        String TS = S.trim();
-        System.out.println(TS); // DilankaRW
+        String S1 = "Cool";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "Cohhhhhhhhhol";
+        S2 = S2.replaceAll("h","");
+        System.out.println(S2);
+
+        String S3 = "   Cool     ";
+        S3 = S3.trim();
+        System.out.println(S3);
+
+        String S4 = "        Cool";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
+
+        String S5 = "Good        ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
+        String[] S6 = {"D","F","K","L","Y","A"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        Integer[] S7 = {232,4,5656,78,898,32,56,8};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
     }
 }

96a1a5c947187022d0cc7f6be692c62bb02c0bf4
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-26 23:19:03 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P30.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P30.java
@@ -1,12 +1,13 @@
-// Sort Array
+// Array Sum
 package PracticeFile1;
 
 import java.util.Arrays;
 
 public class P30 {
     public static void main(String[] args) {
-        int[] num = {54,84,65,8,4,54,58,545,2335,86,655,};
-        Arrays.sort(num);
-        System.out.println(Arrays.toString(num)); // [4, 8, 54, 54, 58, 65, 84, 86, 545, 655, 2335]
+        int[] num = {34,57,89,34,67};
+        int sum = 0;
+        for (int i : num) sum += i;
+        System.out.println(sum);
     }
 }

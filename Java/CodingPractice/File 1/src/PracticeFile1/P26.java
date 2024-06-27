137941a50dc5b4a0d6f72a23a4cac01668b1e272
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-27 16:01:40 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P26.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P26.java
@@ -1,13 +1,48 @@
-// Array Sum
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+
 package PracticeFile1;
 
 import java.util.Arrays;
 
 public class P26 {
     public static void main(String[] args) {
-        int[] num = {1, 2, 3, 4, 6};
+        String S1 = "roof";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "roof";
+        S2 = S2.replaceAll("o","");
+        System.out.println(S2);
+
+        String S3 = "        roof           ";
+        S3 = S3.trim();
+        System.out.println(S3);
+
+        String S4 = "         roof";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
+
+        String S5 = "roof        ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
+        String[] S6 = {"r","o","o","f"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        int[] S7 = {9,8,7,6,5,4,3,2,1};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {5,10,15};
         int sum = 0;
-        for ( int i : num) sum += i;
+        for (int i : S8) sum += i;
         System.out.println(sum);
     }
 }

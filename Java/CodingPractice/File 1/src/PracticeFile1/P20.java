6ac47cc91b34c23e5171295429fc89329db9bcb3
DilankaRW <64007601+DilankaRW@users.noreply.github.com>
2024-06-28 19:59:09 +0530

    Update P20.java

--- Java/CodingPractice/File 1/src/PracticeFile1/P20.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P20.java
@@ -4,39 +4,45 @@
 // Remove leading spaces from a string
 // Remove trailing spaces from a string
 // Sort Array
+// Sum Array
 
 package PracticeFile1;
 
 import java.util.Arrays;
 
-public class P20 {
+public class P30 {
     public static void main(String[] args) {
-        String S1 = "google";
+        String S1 = "food";
         S1 = new StringBuilder(S1).reverse().toString();
         System.out.println(S1);
 
-        String S2 = "google";
-        S2 = S2.replaceAll("g","");
+        String S2 = "food";
+        S2 = S2.replaceAll("o","");
         System.out.println(S2);
 
-        String S3 = "       google    ";
+        String S3 = "        food    ";
         S3 = S3.trim();
         System.out.println(S3);
 
-        String S4 = "        google";
+        String S4 = "         food";
         S4 = S4.replaceAll("^\\s+","");
         System.out.println(S4);
 
-        String S5 = "google     ";
+        String S5 = "food     ";
         S5 = S5.replaceAll("\\s+$","");
         System.out.println(S5);
 
-        String[] S6 = {"g","o","o","g","l","e"};
+        String[] S6 = {"f","o","o","d"};
         Arrays.sort(S6);
         System.out.println(Arrays.toString(S6));
 
-        int[] S7 = {9,7,3,5,8};
+        int[] S7 = {5,432,32,6,778};
         Arrays.sort(S7);
         System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {45,8,89,32,67,9};
+        int sum = 0;
+        for (int i : S8) sum += i;
+        System.out.println(sum);
     }
 }

4899d98c78b19f1cd6b3b741d565871c90f0fa8a
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-29 23:03:52 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P20.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P20.java
@@ -4,6 +4,7 @@
 // Remove leading spaces from a string
 // Remove trailing spaces from a string
 // Sort Array
+// Sum Array
 
 package PracticeFile1;
 
@@ -38,5 +39,10 @@ public class P20 {
         int[] S7 = {9,7,3,5,8};
         Arrays.sort(S7);
         System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {3,5,7,9};
+        int sum = 0;
+        for (int i : S8) sum += i;
+        System.out.println(sum);
     }
 }

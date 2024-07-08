57e73269b4b5f39a7ed4f79dff5120d0ba6cdb05
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-08 23:59:34 +0530

    Update

---
+++ Spring Boot/Project 2/frontend/src/layout/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand" to="/">User Data Management System</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                {/* <Link className='btn btn-light' to="/adduser">Add User</Link> */}
+            </div>
+        </nav>
+    </div>
+  )
+}

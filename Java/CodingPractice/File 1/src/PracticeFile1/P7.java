28430a0b072c08c7a84ec9cd51f5c151de4bb21b
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-12 23:57:15 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P7.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P7.java
@@ -5,8 +5,14 @@
 // Remove trailing spaces from a string
 // Sort Array
 // Sum Array
+// Descending order Array
 
 package PracticeFile1;
 
 public class P7 {
+    public static void main(String[] args) {
+        String S1 = "ring";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+    }
 }

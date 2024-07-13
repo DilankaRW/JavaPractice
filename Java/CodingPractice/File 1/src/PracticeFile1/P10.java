cc3383fe3ac3fd06074847640859460ae0181305
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-13 23:47:52 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P10.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P10.java
@@ -9,6 +9,9 @@
 
 package PracticeFile1;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class P10 {
     public static void main(String[] args) {
         String S1 = "sun";
@@ -30,5 +33,22 @@ public class P10 {
         String S5 = "sun     ";
         S5 = S5.replaceAll("\\s+$","");
         System.out.println(S5);
+
+        String[] S6 = {"s","u","n"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        int[] S7 = {6,4,3,23,83,2,1};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {5,7,8,3};
+        int sum = 0;
+        for (int i : S8) sum += i;
+        System.out.println(sum);
+
+        Integer[] S9 = {3,5,7,9};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
     }
 }

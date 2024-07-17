2bd36609c5d395d5f1ad3a06266c058f0ef574f5
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-17 23:37:46 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P16.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P16.java
@@ -8,6 +8,10 @@
 // Descending order Array
 
 package PracticeFile1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class P16 {
     public static void main(String[] args) {
         String S1 = "jio";
@@ -25,5 +29,26 @@ public class P16 {
         String S4 = "     jio";
         S4 = S4.replaceAll("^\\s+","");
         System.out.println(S4);
+
+        String S5 = "jio    ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
+        String[] S6 = {"j","i","o"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        Integer[] S7 = {9,7,6,3,1};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {5,7,9};
+        int sum = 0;
+        for (int i : S8) sum += i;
+        System.out.println(sum);
+
+        Integer[] S9 = {56,8,89,23};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
     }
 }

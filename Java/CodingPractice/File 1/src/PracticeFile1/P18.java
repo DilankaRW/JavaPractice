b6cd18838b1f7281ce970bbe3670b661696fe4ed
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-18 13:07:51 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P18.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P18.java
@@ -5,7 +5,50 @@
 // Remove trailing spaces from a string
 // Sort Array
 // Sum Array
+// Descending order Array
 
 package PracticeFile1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class P18 {
+    public static void main(String[] args) {
+        String S1 = "zora";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "zora";
+        S2 = S2.replaceAll("a","");
+        System.out.println(S2);
+
+        String S3 = "   zora   ";
+        S3 = S3.trim();
+        System.out.println(S3);
+
+        String S4 = "    zora";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
+
+        String S5 = "zora   ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
+        String[] S6 = {"z","o","r","a"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        int[] S7 = {8,6,5,3,1,9};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {6,8,3};
+        int sum = 0;
+        for (int i : S8) sum += i;
+        System.out.println(sum);
+
+        Integer[] S9 = {5,7,9};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
+    }
 }

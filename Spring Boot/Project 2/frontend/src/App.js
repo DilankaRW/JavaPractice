501296f86c4903827c3249cee3319b763e3961cf
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-18 23:59:44 +0530

    Update

--- Spring Boot/Project 2/frontend/src/App.js
+++ Spring Boot/Project 2/frontend/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar/>
       <Home/>
+      </Router>
     </div>
   );
 }

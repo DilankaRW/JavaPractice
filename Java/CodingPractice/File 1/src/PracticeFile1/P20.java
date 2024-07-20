a5b7b98aaf245cfc15126819db368d8e728ff409
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-21 00:01:22 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P20.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P20.java
@@ -5,7 +5,21 @@
 // Remove trailing spaces from a string
 // Sort Array
 // Sum Array
+// Descending order Array
 
 package PracticeFile1;
 public class P20 {
+    public static void main(String[] args) {
+        String S1 = "salo";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "salo";
+        S2 = S2.replaceAll("o","");
+        System.out.println(S2);
+
+        String S3 = "  salo  ";
+        S3 = S3.trim();
+        System.out.println(S3);
+    }
 }
--- Java/CodingPractice/File 1/src/PracticeFile1/P21.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P21.java
@@ -7,42 +7,5 @@
 // Sum Array
 
 package PracticeFile1;
-
-import java.util.Arrays;
-
 public class P21 {
-    public static void main(String[] args) {
-        String S1 = "sigiriya";
-        S1 = new StringBuilder(S1).reverse().toString();
-        System.out.println(S1);
-
-        String S2 = "sigiriya";
-        S2 = S2.replaceAll("i","");
-        System.out.println(S2);
-
-        String S3 = "       sigiriya   ";
-        S3 = S3.trim();
-        System.out.println(S3);
-
-        String S4 = "         sigiriya";
-        S4 = S4.replaceAll("^\\s+","");
-        System.out.println(S4);
-
-        String S5 = "sigiriya       ";
-        S5 = S5.replaceAll("\\s+$","");
-        System.out.println(S5);
-
-        String[] S6 = {"s","i","g","i","r","i","y","a"};
-        Arrays.sort(S6);
-        System.out.println(Arrays.toString(S6));
-
-        int[] S7 = {8,7,4,2};
-        Arrays.sort(S7);
-        System.out.println(Arrays.toString(S7));
-
-        int[] S8 = {34,6,10};
-        int sum = 0;
-        for (int i : S8) sum += i;
-        System.out.println(sum);
-    }
 }

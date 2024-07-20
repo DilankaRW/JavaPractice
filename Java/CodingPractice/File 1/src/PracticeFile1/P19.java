f738d5fc15c5b2d1f9b9c9ef2ad16e510aad96ff
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-20 23:51:40 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P19.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P19.java
@@ -8,6 +8,10 @@
 // Descending order Array
 
 package PracticeFile1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class P19 {
     public static void main(String[] args) {
         String S1 = "sora";
@@ -25,5 +29,26 @@ public class P19 {
         String S4 = "    sora";
         S4 = S4.replaceAll("^\\s+","");
         System.out.println(S4);
+
+        String S5 = "sora    ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
+        String[] S6 = {"s","o","r","a"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        int[] S7 = {8,6,4,2,9,1};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {4,6,8};
+        int sum = 0;
+        for (int i : S8) sum +=i;
+        System.out.println(sum);
+
+        Integer[] S9 = {4,6,8};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
     }
 }
--- Java/CodingPractice/File 1/src/PracticeFile1/P20.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P20.java
@@ -7,42 +7,5 @@
 // Sum Array
 
 package PracticeFile1;
-
-import java.util.Arrays;
-
 public class P20 {
-    public static void main(String[] args) {
-        String S1 = "food";
-        S1 = new StringBuilder(S1).reverse().toString();
-        System.out.println(S1);
-
-        String S2 = "food";
-        S2 = S2.replaceAll("o","");
-        System.out.println(S2);
-
-        String S3 = "        food    ";
-        S3 = S3.trim();
-        System.out.println(S3);
-
-        String S4 = "         food";
-        S4 = S4.replaceAll("^\\s+","");
-        System.out.println(S4);
-
-        String S5 = "food     ";
-        S5 = S5.replaceAll("\\s+$","");
-        System.out.println(S5);
-
-        String[] S6 = {"f","o","o","d"};
-        Arrays.sort(S6);
-        System.out.println(Arrays.toString(S6));
-
-        int[] S7 = {5,432,32,6,778};
-        Arrays.sort(S7);
-        System.out.println(Arrays.toString(S7));
-
-        int[] S8 = {3,5,7,9};
-        int sum = 0;
-        for (int i : S8) sum += i;
-        System.out.println(sum);
-    }
 }

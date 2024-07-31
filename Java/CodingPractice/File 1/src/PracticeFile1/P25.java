fab2ca43e2f77679842bf0d83789251df8da28fa
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-31 23:46:10 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P25.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P25.java
@@ -9,6 +9,8 @@
 
 package PracticeFile1;
 
+import java.util.Arrays;
+
 public class P25 {
     public static void main(String[] args) {
         String S1 = "ato";
@@ -22,5 +24,17 @@ public class P25 {
         String S3 = "   ato     ";
         S3 = S3.trim();
         System.out.println(S3);
+
+        String S4 = "    ato";
+        S4 = S4.replaceAll("^\\s+" ,"");
+        System.out.println(S4);
+
+        String S5 = "ato     ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
+        String[] S6 = {"a","t","o"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
     }
 }

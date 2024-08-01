5ba36ec571209dcc092527da2effb563a51837e3
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-01 19:01:23 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P26.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P26.java
@@ -1,3 +1,11 @@
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
 // Shuffle an array
 
 package PracticeFile1;
@@ -8,6 +16,19 @@ import java.util.List;
 
 public class P26 {
     public static void main(String[] args) {
+
+        String S1 = "zxc";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "zxc";
+        S2 = S2.replaceAll("x","");
+        System.out.println(S2);
+
+        String S3 = "    zxc   ";
+        S3 = S3.trim();
+        System.out.println(S3);
+
         Integer[] array = {45,678,23,65,4};
         List<Integer> List = Arrays.asList(array);
         Collections.shuffle(List);

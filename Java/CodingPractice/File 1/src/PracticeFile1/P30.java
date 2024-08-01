c4722b638127f0dc78ae8395a41ff7b5936a0459
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-01 18:53:59 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P30.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P30.java
@@ -1,54 +1,17 @@
-// String Reverse
-// Remove Character
-// Remove leading and trailing spaces from a string.
-// Remove leading spaces from a string
-// Remove trailing spaces from a string
-// Sort Array
-// Sum Array
-// Descending order Array
+// Shuffle an array
 
 package PracticeFile1;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class P30 {
     public static void main(String[] args) {
-        String S1 = "food";
-        S1 = new StringBuilder(S1).reverse().toString();
-        System.out.println(S1);
-
-        String S2 = "food";
-        S2 = S2.replaceAll("o","");
-        System.out.println(S2);
-
-        String S3 = "        food    ";
-        S3 = S3.trim();
-        System.out.println(S3);
-
-        String S4 = "         food";
-        S4 = S4.replaceAll("^\\s+","");
-        System.out.println(S4);
-
-        String S5 = "food     ";
-        S5 = S5.replaceAll("\\s+$","");
-        System.out.println(S5);
-
-        String[] S6 = {"f","o","o","d"};
-        Arrays.sort(S6);
-        System.out.println(Arrays.toString(S6));
-
-        int[] S7 = {5,432,32,6,778};
-        Arrays.sort(S7);
-        System.out.println(Arrays.toString(S7));
-
-        int[] S8 = {45,8,89,32,67,9};
-        int sum = 0;
-        for (int i : S8) sum += i;
-        System.out.println(sum);
-
-        Integer[] S9 = {1,2,3,4,5,6,7,8,9};
-        Arrays.sort(S9, Collections.reverseOrder());
-        System.out.println(Arrays.toString(S9));
+        Integer[] S10 = {35,56,89,23,98};
+        List<Integer> list = Arrays.asList(S10);
+        Collections.shuffle(list);
+        list.toArray(S10);
+        System.out.println(Arrays.toString(S10));
     }
 }

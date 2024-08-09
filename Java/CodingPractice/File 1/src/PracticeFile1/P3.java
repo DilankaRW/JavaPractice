da1c19a9a97306db3e75b53d0f2e11ff9d3ba352
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-10 00:32:22 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P3.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P3.java
@@ -10,10 +10,14 @@
 
 package PracticeFile1;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class P3 {
     public static void main(String[] args) {
         String S1 = "are";
-        S1 = new StringBuilder(S1).reverse().toString()
+        S1 = new StringBuilder(S1).reverse().toString();
         System.out.println(S1);
 
         String S2 = "are";
@@ -23,5 +27,36 @@ public class P3 {
         String S3 = "    are   ";
         S3 = S3.trim();
         System.out.println(S3);
+
+        String S4 = "   are";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
+
+        String S5 = "are    ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
+        String[] S6 = {"a","r","e"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        int[] S7 = {3,5,779,34,56,78,90};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {45,674,34};
+        int sum = 0;
+        for (int i : S8) sum += i;
+        System.out.println(sum);
+
+        Integer[] S9 = {4,78,5,575,87,15,18};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
+
+        Integer[] S10 = {4,5,89,75,2,668,87,21,57};
+        List<Integer> list = Arrays.asList(S10);
+        Collections.shuffle(list);
+        list.toArray(S10);
+        System.out.println(Arrays.toString(S10));
     }
 }

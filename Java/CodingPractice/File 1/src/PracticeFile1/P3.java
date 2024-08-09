c08940fa775e2ccc7117c591433d95881fff7cb5
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-09 23:59:02 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P3.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P3.java
@@ -11,4 +11,17 @@
 package PracticeFile1;
 
 public class P3 {
+    public static void main(String[] args) {
+        String S1 = "are";
+        S1 = new StringBuilder(S1).reverse().toString()
+        System.out.println(S1);
+
+        String S2 = "are";
+        S2 = S2.replaceAll("r","");
+        System.out.println(S2);
+
+        String S3 = "    are   ";
+        S3 = S3.trim();
+        System.out.println(S3);
+    }
 }

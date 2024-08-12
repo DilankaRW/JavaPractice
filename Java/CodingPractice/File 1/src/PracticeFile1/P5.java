6b8d69e845270afdd0b91509400c508a850216e6
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-12 23:49:35 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P5.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P5.java
@@ -11,4 +11,17 @@
 package PracticeFile1;
 
 public class P5 {
+    public static void main(String[] args) {
+        String S1 = "glo";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "glo";
+        S2 = S2.replaceAll("l","");
+        System.out.println(S2);
+
+        String S3 = "    glo   ";
+        S3 = S3.trim();
+        System.out.println(S3);
+    }
 }

bcb3be39ff9aab6f0e4e03a7e8a0e1dfaf18d7a5
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-18 20:55:00 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P17.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P17.java
@@ -10,4 +10,17 @@
 package PracticeFile1;
 
 public class P17 {
+    public static void main(String[] args) {
+        String S1 = "era";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "era";
+        S2 = S2.replaceAll("e","");
+        System.out.println(S2);
+
+        String S3 = "   era    ";
+        S3 = S3.trim();
+        System.out.println(S3);
+    }
 }

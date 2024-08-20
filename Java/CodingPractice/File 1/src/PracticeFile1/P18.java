ed62fb355c31852184f9cee88a0cd0e1cb24a971
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-20 10:33:01 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P18.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P18.java
@@ -6,11 +6,13 @@
 // Sort Array
 // Sum Array
 // Descending order Array
+// Shuffle an array
 
 package PracticeFile1;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class P18 {
     public static void main(String[] args) {
@@ -50,5 +52,11 @@ public class P18 {
         Integer[] S9 = {5,7,9};
         Arrays.sort(S9, Collections.reverseOrder());
         System.out.println(Arrays.toString(S9));
+
+        Integer[] S10 = {45,7,5,322,23};
+        List<Integer> list = Arrays.asList(S10);
+        Collections.shuffle(list);
+        list.toArray(S10);
+        System.out.println(Arrays.toString(S10));
     }
 }
--- Java/CodingPractice/File 1/src/PracticeFile1/P19.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P19.java
@@ -6,49 +6,9 @@
 // Sort Array
 // Sum Array
 // Descending order Array
+// Shuffle an array
 
 package PracticeFile1;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 public class P19 {
-    public static void main(String[] args) {
-        String S1 = "sora";
-        S1 = new StringBuilder(S1).reverse().toString();
-        System.out.println(S1);
-
-        String S2 = "sora";
-        S2 = S2.replaceAll("a","");
-        System.out.println(S2);
-
-        String S3 = "    sora   ";
-        S3 = S3.trim();
-        System.out.println(S3);
-
-        String S4 = "    sora";
-        S4 = S4.replaceAll("^\\s+","");
-        System.out.println(S4);
-
-        String S5 = "sora    ";
-        S5 = S5.replaceAll("\\s+$","");
-        System.out.println(S5);
-
-        String[] S6 = {"s","o","r","a"};
-        Arrays.sort(S6);
-        System.out.println(Arrays.toString(S6));
-
-        int[] S7 = {8,6,4,2,9,1};
-        Arrays.sort(S7);
-        System.out.println(Arrays.toString(S7));
-
-        int[] S8 = {4,6,8};
-        int sum = 0;
-        for (int i : S8) sum +=i;
-        System.out.println(sum);
-
-        Integer[] S9 = {4,6,8};
-        Arrays.sort(S9, Collections.reverseOrder());
-        System.out.println(Arrays.toString(S9));
-    }
 }

a475316d5a3301a30b281393071324eaf94d5ba7
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-27 21:30:30 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P18.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P18.java
@@ -12,4 +12,25 @@
 package PracticeFile1;
 
 public class P18 {
+    public static void main(String[] args) {
+        String S1 = "dsa";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "dsa";
+        S2 = S2.replaceAll("s","");
+        System.out.println(S2);
+
+        String S3 = "     dsa   ";
+        S3 = S3.trim();
+        System.out.println(S3);
+
+        String S4 = "   dsa";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
+
+        String S5 = "dsa    ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+    }
 }

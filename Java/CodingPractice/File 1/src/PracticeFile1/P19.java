a04091d19f3b7bffd93c6616a20186783c5bb3cb
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-27 21:56:56 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P19.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P19.java
@@ -11,6 +11,10 @@
 
 package PracticeFile1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class P19 {
     public static void main(String[] args) {
         String S1 = "qwe";
@@ -20,7 +24,6 @@ public class P19 {
         String S2 = "qwe";
         S2 = S2.replaceAll("w","");
         System.out.println(S2);
-
         String S3 = "    qwe   ";
         S3 = S3.trim();
         System.out.println(S3);
@@ -32,5 +35,22 @@ public class P19 {
         String S5 = "qwe    ";
         S5 = S5.replaceAll("\\s+$","");
         System.out.println(S5);
+
+        String[] S6 = {"q","w","e"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        int[] S7 = {56,876,32,21};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {56,34,21};
+        int sum = 0;
+        for (int i : S8) sum += i;
+        System.out.println(sum);
+
+        Integer[] S9 = {45,57,4,23,75};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
     }
 }

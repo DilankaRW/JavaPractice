8620af30d263617cde450a3b55e531e4b8bbf843
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-27 21:49:11 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P19.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P19.java
@@ -7,8 +7,30 @@
 // Sum Array
 // Descending order Array
 // Shuffle an array
+// Create a pyramid of characters.
 
 package PracticeFile1;
 
 public class P19 {
+    public static void main(String[] args) {
+        String S1 = "qwe";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "qwe";
+        S2 = S2.replaceAll("w","");
+        System.out.println(S2);
+
+        String S3 = "    qwe   ";
+        S3 = S3.trim();
+        System.out.println(S3);
+
+        String S4 = "     qwe";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
+
+        String S5 = "qwe    ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+    }
 }

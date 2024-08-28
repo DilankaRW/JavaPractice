b17ae699c2bf88dab8ffdc418b10a9aaf31f078b
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-28 20:35:41 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P21.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P21.java
@@ -6,11 +6,13 @@
 // Sort Array
 // Sum Array
 // Descending order Array
+// Shuffle an array
 
 package PracticeFile1;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class P21 {
     public static void main(String[] args) {
@@ -50,5 +52,11 @@ public class P21 {
         Integer[] S9 = {56,8,89,34,7};
         Arrays.sort(S9, Collections.reverseOrder());
         System.out.println(Arrays.toString(S9));
+
+        Integer[] S10 = {34,542,3,23};
+        List<Integer> list = Arrays.asList(S10);
+        Collections.shuffle(list);
+        list.toArray(S10);
+        System.out.println(Arrays.toString(S10));
     }
 }

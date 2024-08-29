58520fb197c7e8ed5f4ac63cb1dcd64689dc65bf
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-29 17:13:15 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P29.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P29.java
@@ -1,27 +1,56 @@
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
 // Create a pyramid of characters.
 
 package PracticeFile1;
 
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class P29 {
     public static void main(String[] args) {
-        Scanner s  = new Scanner(System.in);
+        String S1 = "vbn";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
 
-        System.out.print("Rows Count : ");
-        int rows = s.nextInt();
-        s.close();
+        String S2 = "vbn";
+        S2 = S2.replaceAll("v","");
+        System.out.println(S2);
 
-        for (int i = 1; i <= rows; i++){
-            for (int x = rows - i; x > 0; x--){
-                System.out.print(" ");
-            }
+        String S3 = "   vbn    ";
+        S3 = S3.trim();
+        System.out.println(S3);
 
-            for (int x = 1; x <= (2 * i - 1); x++){
-                System.out.print("*");
-            }
+        String S4 = "   vbn";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
 
-            System.out.println();
-        }
+        String S5 = "vbn   ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
+        String[] S6 = {"v","b","n"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        Integer[] S7 = {67,45,32};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {6,4,2};
+        int sum = 0;
+        for (int i : S8) sum += i;
+        System.out.println(sum);
+
+        Integer[] S9 = {24,457,89};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
     }
 }

050f0cc0b798ee8b2b4b0a15dc7ea40d22a489d8
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-29 16:52:23 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile1/P27.java
+++ Java/CodingPractice/File 1/src/PracticeFile1/P27.java
@@ -1,26 +1,36 @@
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
 // Create a pyramid of characters.
 
 package PracticeFile1;
 
-import java.util.Scanner;
-
 public class P27 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        String S1 = "sdf";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
 
-        System.out.print("Rows Count : ");
-        int rows = s.nextInt();
-        s.close();
+        String S2 = "sdf";
+        S2 = S2.replaceAll("d","");
+        System.out.println(S2);
 
-        for (int i = 1; i<= rows; i++){
-            for (int x = rows - i; x> 0; x--){
-                System.out.print(" ");
-            }
-            for (int x = 1; x <= (2 * i - 1); x++){
-                System.out.print("*");
-            }
+        String S3 = "    sdf   ";
+        S3 = S3.trim();
+        System.out.println(S3);
 
-            System.out.println();
-        }
+        String S4 = "     sdf";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
+
+        String S5 = "sdf    ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
     }
 }

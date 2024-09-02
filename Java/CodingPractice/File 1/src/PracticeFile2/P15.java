5634e3ac0cbc3a8b56f20e29c5d8b46617836bc1
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-02 14:14:26 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile2/P15.java
@@ -0,0 +1,23 @@
+// Create a pyramid of characters.
+
+package PracticeFile2;
+
+import java.util.Scanner;
+
+public class P15 {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.print("Rows Count : ");
+        int row = S.nextInt();
+        S.close();
+        for (int i=1; i<=row; i++){
+            for (int x=row-i; x>0; x--){
+                System.out.print(" ");
+            }
+            for (int x=1; x<=(2*i-1); x++){
+                System.out.print("7");
+            }
+            System.out.println();
+        }
+    }
+}

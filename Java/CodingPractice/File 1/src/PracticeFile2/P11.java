32f8ba60ed29911459643cc31b52e09fe21e16a3
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-12 23:43:50 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile2/P11.java
+++ Java/CodingPractice/File 1/src/PracticeFile2/P11.java
@@ -1,3 +1,12 @@
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
 // Create a pyramid of characters.
 
 package PracticeFile2;
@@ -6,6 +15,26 @@ import java.util.Scanner;
 
 public class P11 {
     public static void main(String[] args) {
+        String S1 = "front";
+        S1 = new StringBuilder(S1).reverse().toString();;
+        System.out.println(S1);
+
+        String S2 = "front";
+        S2 = S2.replaceAll("o","*");
+        System.out.println(S2);
+
+        String S3 = "   front   ";
+        S3 = S3.trim();
+        System.out.println(S3);
+
+        String S4 = "    front";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
+
+        String S5 = "front    ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
         Scanner S = new Scanner(System.in);
         System.out.print("Rows Count : ");
         int row = S.nextInt();

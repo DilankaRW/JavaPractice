142ade6a817b33b8e3f6b7575d404205b68b3d84
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-17 23:43:54 +0530

    String Reverse

--- Java/CodingPractice/File 1/src/PracticeFile2/P14.java
+++ Java/CodingPractice/File 1/src/PracticeFile2/P14.java
@@ -1,3 +1,12 @@
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
 // Create a pyramid of characters.
 
 package PracticeFile2;
@@ -6,18 +15,8 @@ import java.util.Scanner;
 
 public class P14 {
     public static void main(String[] args) {
-        Scanner S = new Scanner(System.in);
-        System.out.print("Rows Count : ");
-        int row = S.nextInt();
-        S.close();
-        for (int i=1; i<=row; i++){
-            for (int x=row-i; x>0; x--){
-                System.out.print(" ");
-            }
-            for (int x=1; x<=(2*i-1); x++){
-                System.out.print("^");
-            }
-            System.out.println();
-        }
+        String S1 = "develop";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
     }
 }

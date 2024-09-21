dbec3fca63bba558d30063cb69062bda8008b84d
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-21 15:30:51 +0530

    String Reverse / Remove Character

---
+++ Java/CodingPractice/File 1/src/PracticeFile2/P27.java
@@ -0,0 +1,24 @@
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
+// Create a pyramid of characters.
+
+package PracticeFile2;
+
+public class P27 {
+    public static void main(String[] args) {
+        String S1 = "real";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "real";
+        S2 = S2.replaceAll("a","");
+        System.out.println(S2);
+    }
+}

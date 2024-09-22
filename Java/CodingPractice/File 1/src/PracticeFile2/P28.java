353f1af622218db31179da9058a7ddc664c0a014
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-22 23:47:06 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile2/P28.java
@@ -0,0 +1,28 @@
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
+// Create a pyramid of characters.
+
+package PracticeFile2;
+
+public class P28 {
+    public static void main(String[] args) {
+        String S1 = "world";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "world";
+        S2 = S2.replaceAll("r","*");
+        System.out.println(S2);
+
+        String S3 = "      world     ";
+        S3 = S3.trim();
+        System.out.println(S3);
+    }
+}

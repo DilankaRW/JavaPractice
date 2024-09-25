bece2a05aaa8a0f23f38bf78b42cbd43939c05c0
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-26 00:00:01 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile2/P16.java
+++ Java/CodingPractice/File 1/src/PracticeFile2/P16.java
@@ -20,16 +20,30 @@ public class P16 {
 //            System.out.println();
 //        }
 
-        Scanner S1 = new Scanner(System.in);
+//        Scanner S1 = new Scanner(System.in);
+//        System.out.print("Rows Count : ");
+//        int row1 = S1.nextInt();
+//        S1.close();
+//        for (int i=1; i<=row1; i++){
+//            for (int x=row1-i; x>0; x--){
+//                System.out.print(" ");
+//            }
+//            for (int x=1;x<=(2*i-1); x++){
+//                System.out.print("1");
+//            }
+//            System.out.println();
+//        }
+
+        Scanner S2 = new Scanner(System.in);
         System.out.print("Rows Count : ");
-        int row1 = S1.nextInt();
-        S1.close();
-        for (int i=1; i<=row1; i++){
-            for (int x=row1-i; x>0; x--){
+        int row = S2.nextInt();
+        S2.close();
+        for (int i=1; i<=row; i++){
+            for (int x=row-i; x>0; x--){
                 System.out.print(" ");
             }
-            for (int x=1;x<=(2*i-1); x++){
-                System.out.print("1");
+            for (int x=1; x<=(2*i-1); x++){
+                System.out.print("*");
             }
             System.out.println();
         }

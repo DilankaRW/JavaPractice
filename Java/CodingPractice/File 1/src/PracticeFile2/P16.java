7bc2fbfa0e06fba217d0745b6141de96d70966d5
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-25 23:55:50 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile2/P16.java
+++ Java/CodingPractice/File 1/src/PracticeFile2/P16.java
@@ -6,16 +6,30 @@ import java.util.Scanner;
 
 public class P16 {
     public static void main(String[] args) {
-        Scanner S = new Scanner(System.in);
+//        Scanner S = new Scanner(System.in);
+//        System.out.print("Rows Count : ");
+//        int row = S.nextInt();
+//        S.close();
+//        for (int i=1; i<=row; i++){
+//            for (int x=row-i; x>0; x--){
+//                System.out.print(" ");
+//            }
+//            for (int x=1; x<=(2*i-1); x++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        Scanner S1 = new Scanner(System.in);
         System.out.print("Rows Count : ");
-        int row = S.nextInt();
-        S.close();
-        for (int i=1; i<=row; i++){
-            for (int x=row-i; x>0; x--){
+        int row1 = S1.nextInt();
+        S1.close();
+        for (int i=1; i<=row1; i++){
+            for (int x=row1-i; x>0; x--){
                 System.out.print(" ");
             }
-            for (int x=1; x<=(2*i-1); x++){
-                System.out.print("*");
+            for (int x=1;x<=(2*i-1); x++){
+                System.out.print("1");
             }
             System.out.println();
         }

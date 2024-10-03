10da740c624e389fb5244715636b57bf2de0932c
VL.dilankarw <dilankarashmika10@gmail.com>
2024-10-03 23:28:33 +0530

    Descending order Array

--- Java/CodingPractice/File 1/src/PracticeFile2/P16.java
+++ Java/CodingPractice/File 1/src/PracticeFile2/P16.java
@@ -12,6 +12,7 @@
 package PracticeFile2;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class P16 {
@@ -49,5 +50,9 @@ public class P16 {
         int sum = 0;
         for (int i : S8) sum += i;
         System.out.println(sum);
+
+        Integer[] S9 = {45,7,87,34,21,56};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
     }
 }

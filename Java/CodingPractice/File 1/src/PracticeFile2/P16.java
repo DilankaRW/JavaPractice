3f9d2490aa54144d7264292f461f5d75e343e6f4
VL.dilankarw <dilankarashmika10@gmail.com>
2024-10-03 23:27:38 +0530

    Sum Array

--- Java/CodingPractice/File 1/src/PracticeFile2/P16.java
+++ Java/CodingPractice/File 1/src/PracticeFile2/P16.java
@@ -44,5 +44,10 @@ public class P16 {
         Integer[] S7 = {23,45,78,12,46,21};
         Arrays.sort(S7);
         System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {23,45,87};
+        int sum = 0;
+        for (int i : S8) sum += i;
+        System.out.println(sum);
     }
 }

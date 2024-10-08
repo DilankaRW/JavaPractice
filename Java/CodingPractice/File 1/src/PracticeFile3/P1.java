d3add6b8fb6137d09e24f53616f583a0b2ded62f
VL.dilankarw <dilankarashmika10@gmail.com>
2024-10-08 23:42:33 +0530

    Date in specific format

--- Java/CodingPractice/File 1/src/PracticeFile3/P1.java
+++ Java/CodingPractice/File 1/src/PracticeFile3/P1.java
@@ -12,9 +12,14 @@ public class P1 {
         String date = sdf.format(new Date());
         System.out.println(date);
 
-        String pattern1  = "1dd-MM-yyyy HH:mm:ss z";
+        String pattern1  = "dd-MM-yyyy HH:mm:ss z";
         SimpleDateFormat sdf1 = new SimpleDateFormat(pattern1);
         String date1 = sdf1.format(new Date());
         System.out.println(date1);
+
+        String pattern2 = "yyyy MMMM dd EEEE HH:mm:ss zzzz";
+        SimpleDateFormat sdf2 = new SimpleDateFormat(pattern2);
+        String date2 = sdf2.format(new Date());
+        System.out.println(date2);
     }
 }

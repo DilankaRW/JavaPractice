73c8471cc62ab6c464ae8f5db52f3a7afd4da205
VL.dilankarw <dilankarashmika10@gmail.com>
2024-10-14 10:33:53 +0530

    DELETE User using AXIOS

--- Spring Boot/Project 3/frontend/src/pages/Home.jsx
+++ Spring Boot/Project 3/frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export const Home = () => {
   const [users, setUsers] = useState([]);
+
+  const { id } = useParams();
 
   useEffect(() => {
     loadUsers();
@@ -12,6 +14,11 @@ export const Home = () => {
   const loadUsers = async () => {
     const result = await axios.get("http://localhost:8080/users");
     setUsers(result.data);
+  };
+
+  const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:8080/user/${id}`);
+    loadUsers();
   };
 
   return (
@@ -38,8 +45,13 @@ export const Home = () => {
                 <td>{user.email}</td>
                 <td>
                   <button className="btn btn-primary mx-2">View</button>
-                  <Link className="btn btn-outline-primary mx-2" to={`/edituser/${user.id}`}>Edit</Link>
-                  <button className="btn btn-danger mx-2">Delete</button>
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                    to={`/edituser/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <button className="btn btn-danger mx-2" onClick={() => deleteUser(user.id)}>Delete</button>
                 </td>
               </tr>
             ))}

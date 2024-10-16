6247900043396a8e12173e25ba744452ec09c338
VL.dilankarw <dilankarashmika10@gmail.com>
2024-10-16 23:53:05 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P2.java
@@ -0,0 +1,11 @@
+// String Reverse
+
+package PracticeFile3;
+
+public class P2 {
+    public static void main(String[] args) {
+        String S1  = "navbar";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+    }
+}

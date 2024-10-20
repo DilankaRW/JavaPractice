26c46a080e39953f545119b2eb5ccd75c1be9884
VL.dilankarw <dilankarashmika10@gmail.com>
2024-10-20 23:45:54 +0530

    Shuffle an array

--- Java/CodingPractice/File 1/src/PracticeFile3/P3.java
+++ Java/CodingPractice/File 1/src/PracticeFile3/P3.java
@@ -13,6 +13,7 @@ package PracticeFile3;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class P3 {
     public static void main(String[] args) {
@@ -52,5 +53,11 @@ public class P3 {
         Integer[] S9 = {65,3,78,23,76,22};
         Arrays.sort(S9, Collections.reverseOrder());
         System.out.println(Arrays.toString(S9));
+
+        Integer[] S10 = {34,65,76,23,45};
+        List<Integer> list = Arrays.asList(S10);
+        Collections.shuffle(list);
+        list.toArray(S10);
+        System.out.println(Arrays.toString(S10));
      }
 }

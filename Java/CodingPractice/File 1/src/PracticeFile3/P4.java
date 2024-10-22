fb57b28826fb706f682c5d948bb5ef752069b8bf
VL.dilankarw <dilankarashmika10@gmail.com>
2024-10-22 23:24:49 +0530

    Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P4.java
@@ -0,0 +1,20 @@
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
+// Create a pyramid of characters.
+
+package PracticeFile3;
+
+public class P4 {
+    public static void main(String[] args) {
+        String S1 = "ola";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+    }
+}

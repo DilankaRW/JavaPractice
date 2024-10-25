aa525b93bbef7626f918695b135d78a910b26326
VL.dilankarw <dilankarashmika10@gmail.com>
2024-10-25 20:11:33 +0530

    pyramid of characters.

--- Java/CodingPractice/File 1/src/PracticeFile3/P4.java
+++ Java/CodingPractice/File 1/src/PracticeFile3/P4.java
@@ -14,6 +14,7 @@ package PracticeFile3;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class P4 {
     public static void main(String[] args) {
@@ -59,5 +60,19 @@ public class P4 {
         Collections.shuffle(list);
         list.toArray(S10);
         System.out.println(Arrays.toString(S10));
+
+        Scanner S = new Scanner(System.in);
+        System.out.print("Rows Count : ");
+        int row = S.nextInt();
+        S.close();
+        for (int i=1; i<=row; i++){
+            for (int x=row-i; x>0; x--){
+                System.out.print(" ");
+            }
+            for (int x=1; x<=(2*i-1); x++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }

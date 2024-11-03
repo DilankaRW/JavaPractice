5e28503ad2f1ca769cb95d2a09efc6c37a2c483c
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-04 01:12:49 +0530

    DELETE User using AXIOS

--- Spring Boot/Project 4/frontend/src/pages/Home.jsx
+++ Spring Boot/Project 4/frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export const Home = () => {
   const [users, setUsers] = useState([]);
+
+  const { id } = useParams();
 
   useEffect(() => {
     loadUsers();
@@ -12,6 +14,11 @@ export const Home = () => {
   const loadUsers = async () => {
     const result = await axios.get("http://localhost:8080/users");
     setUsers(result.data);
+  };
+
+  const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:8080/user/${id}`);
+    loadUsers();
   };
 
   return (
@@ -30,14 +37,26 @@ export const Home = () => {
           <tbody>
             {users.map((user, index) => (
               <tr>
-                <th scope="row" key={index}>{index + 1}</th>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
                 <td>{user.name}</td>
                 <td>{user.username}</td>
                 <td>{user.email}</td>
                 <td>
                   <button className="btn btn-primary mx-2">View</button>
-                  <Link className="btn btn-outline-primary mx-2" to={`/edituser/${user.id}`}>Edit</Link>
-                  <button className="btn btn-danger mx-2">Delete</button>
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                    to={`/edituser/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-danger mx-2"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}

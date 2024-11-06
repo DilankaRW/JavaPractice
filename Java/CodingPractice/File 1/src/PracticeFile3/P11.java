49b66d91b5a5486664df06c0f41d48f3aabdfbaa
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-07 00:00:02 +0530

    fix

--- Java/CodingPractice/File 1/src/PracticeFile3/P11.java
+++ Java/CodingPractice/File 1/src/PracticeFile3/P11.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 public class P11 {
     public static void main(String[] args) {
-        String S = "yyyy mm dd";
+        String S = "yyyy MM dd";
         SimpleDateFormat sdf = new SimpleDateFormat(S);
         String date = sdf.format(new Date());
         System.out.println(date);

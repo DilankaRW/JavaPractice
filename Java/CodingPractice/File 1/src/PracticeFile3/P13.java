c88d940266a82be211429ac8f261c4edf06bb820
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-07 22:47:37 +0530

    Text blocks in Java

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P13.java
@@ -0,0 +1,15 @@
+// Text blocks in Java.
+
+package PracticeFile3;
+
+public class P13 {
+    public static void main(String[] args) {
+        String TextBlock = """
+                This is a text block.
+                It can span multiple lines.
+                Each new line in the source code is also a new line in the string.
+                """;
+
+        System.out.println(TextBlock);
+    }
+}

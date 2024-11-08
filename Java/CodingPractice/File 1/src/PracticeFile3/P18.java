0fb48726b3e2ddfdb1a3dbfabc79dcde05faf91f
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-09 00:19:46 +0530

    Errors - Runtime Errors

--- Java/CodingPractice/File 1/src/PracticeFile3/P18.java
+++ Java/CodingPractice/File 1/src/PracticeFile3/P18.java
@@ -12,14 +12,39 @@ package PracticeFile3;
 
 
 // Logical Errors
+//public class P18 {
+//    public static void main(String[] args) {
+//        int length = 10;
+//        int width = 5;
+//        // int perimeter = length + width * 2; // Logical Error ( (length + width) * 2; )
+//        // Output - 20
+//        int perimeter = (length + width) * 2;
+//        System.out.println(perimeter);
+//        // Output - 30
+//    }
+//}
+
+import java.util.Scanner;
+
+// Runtime Errors
 public class P18 {
     public static void main(String[] args) {
-        int length = 10;
-        int width = 5;
-        // int perimeter = length + width * 2; // Logical Error ( (length + width) * 2; )
-        // Output - 20
-        int perimeter = (length + width) * 2;
-        System.out.println(perimeter);
-//        Output - 30
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        System.out.print("Enter first number");
+        a = sc.nextInt();
+        System.out.print("Enter second number");
+        b = sc.nextInt();
+        c = a/b;
+        System.out.println("Answer is " + c);
+
+//        Input -
+//        Enter first Number 10
+//        Enter first Number 0
+//        Output Runtime Error
+//
+//        Input -
+//        Enter first Number 4.5
+//        Output Runtime Error
     }
 }

62f85609f5b371ade4e198450bb98c9e3d477034
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-08 23:54:02 +0530

    Errors - Syntax Errors

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P18.java
@@ -0,0 +1,11 @@
+// Errors
+
+package PracticeFile3;
+
+// Syntax Error
+public class P18 {
+    public static void main(String[] args) {
+        int a = 10 // Syntax Error (int a = 10;)
+        b = 5; // Syntax Error (int b = 5;)
+    }
+}

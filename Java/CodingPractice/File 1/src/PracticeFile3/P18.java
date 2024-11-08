95606e0a3d955c8db5470b0797ef573a498ac968
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-08 23:59:28 +0530

    Errors - Logical Errors

--- Java/CodingPractice/File 1/src/PracticeFile3/P18.java
+++ Java/CodingPractice/File 1/src/PracticeFile3/P18.java
@@ -3,9 +3,23 @@
 package PracticeFile3;
 
 // Syntax Error
+//public class P18 {
+//    public static void main(String[] args) {
+//        int a = 10 // Syntax Error (int a = 10;)
+//        b = 5; // Syntax Error (int b = 5;)
+//    }
+//}
+
+
+// Logical Errors
 public class P18 {
     public static void main(String[] args) {
-        int a = 10 // Syntax Error (int a = 10;)
-        b = 5; // Syntax Error (int b = 5;)
+        int length = 10;
+        int width = 5;
+        // int perimeter = length + width * 2; // Logical Error ( (length + width) * 2; )
+        // Output - 20
+        int perimeter = (length + width) * 2;
+        System.out.println(perimeter);
+//        Output - 30
     }
 }

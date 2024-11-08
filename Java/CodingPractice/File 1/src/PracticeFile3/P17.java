0b0c121ed68a6a570473caf848bcd1862b8ec72b
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-08 23:21:52 +0530

    Super keyword for methods

--- Java/CodingPractice/File 1/src/PracticeFile3/P17.java
+++ Java/CodingPractice/File 1/src/PracticeFile3/P17.java
@@ -4,21 +4,44 @@ package PracticeFile3;
 
 // Super keyword for Variables
 
+//class A{
+//    int x = 10;
+//}
+//class B extends A{
+//    int x=15;
+//    public void print(){
+//        System.out.println(x);
+//        // output - 15
+//        System.out.println(super.x); // Super keyword
+//        // output - 10
+//    }
+//}
+//public class P17 {
+//    public static void main(String[] args) {
+//        B obj = new B();
+//        obj.print();
+//    }
+//}
+
+
+// Super keyword for methods
+
 class A{
-    int x = 10;
+    void show(){
+        System.out.println("Hi");
+    }
 }
 class B extends A{
-    int x=15;
-    public void print(){
-        System.out.println(x);
-        // output - 15
-        System.out.println(super.x); // Super keyword
-        // output - 10
+    void show(){
+        super.show(); // Super keyword
+        System.out.println("Hello");
+//        Output - Hi
+//        Hello
     }
 }
 public class P17 {
     public static void main(String[] args) {
         B obj = new B();
-        obj.print();
+        obj.show();
     }
 }

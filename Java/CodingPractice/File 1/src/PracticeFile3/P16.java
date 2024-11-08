9424174c05a2ef92ce4e300ec86632585a47b94b
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-08 22:48:23 +0530

    Mathematical Operators

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P16.java
@@ -0,0 +1,39 @@
+// Mathematical Operators
+
+//Addition (+)
+//Subtraction (-)
+//Multiplication (*)
+//Division (/)
+//Modulus (%)
+
+package PracticeFile3;
+
+public class P16 {
+    public static void main(String[] args) {
+        int x,y,z,answer;
+        x=10;
+        y=5;
+        z=3;
+
+        answer = x+y;
+        System.out.println(answer);
+
+        answer = x-y;
+        System.out.println(answer);
+
+        answer = x*y;
+        System.out.println(answer);
+
+        answer = x/y;
+        System.out.println(answer);
+
+        double a,b,answer1;
+        a=10;
+        b=4;
+        answer1=a/b;
+        System.out.println(answer1);
+
+        answer = x%z;
+        System.out.println(answer);
+    }
+}

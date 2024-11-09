fcee233cf4a63ce5cb0e6fa65fdff0d2bdb7f5fe
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-09 23:49:33 +0530

    Variables and Data Types

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P21.java
@@ -0,0 +1,22 @@
+// Variables and Data Types
+
+package PracticeFile3;
+
+public class P21 {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+
+        double b = 5.15;
+        System.out.println(b);
+
+        char c = 'C';
+        System.out.println(c);
+
+        boolean d = true;
+        System.out.println(d);
+
+        String e = "Hello!";
+        System.out.println(e);
+    }
+}

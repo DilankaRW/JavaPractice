5cc1cd686c1f4c5ddf6e7432eb9b36af573fac50
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-09 23:40:43 +0530

    Logical Operators - AND Operator - &&

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P20.java
@@ -0,0 +1,21 @@
+// Logical Operators
+// AND - &&
+// OR - ||
+
+package PracticeFile3;
+
+public class P20 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        // AND Operator - &&
+
+        if (x>7 && y>7){
+            System.out.println("OK");
+        }else {
+            System.out.println("NO");
+        }
+//        Output - NO
+    }
+}

65677fec8efc96c91c5fb25939cf52ccefb19c69
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-09 23:59:59 +0530

    LinkedList

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P23.java
@@ -0,0 +1,12 @@
+package PracticeFile3;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class P23 {
+    public static void main(String[] args) {
+        LinkedList<Integer> arr = new LinkedList(List.of(1,2,3));
+        arr.add(12);
+        System.out.println(arr);
+    }
+}

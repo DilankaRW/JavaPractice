231df8faf8a5aaf6d73a35e912fe408288811e40
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-10 23:58:19 +0530

    for loop

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P25.java
@@ -0,0 +1,13 @@
+// for loop
+// A for loop is a repetition control structure that allows you to efficiently write a loop that needs to be executed a specific number of times.
+
+package PracticeFile3;
+
+public class P25 {
+    public static void main(String[] args) {
+        for (int i = 1; i<=10; i++){
+            System.out.println(i);
+        }
+        // Output - 1,2,3,4,5,6,7,8,9,10
+    }
+}

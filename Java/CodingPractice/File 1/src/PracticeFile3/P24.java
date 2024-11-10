3fffbc205922bb5738905d3c19787d997680d251
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-10 23:53:34 +0530

    if else Statement

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P24.java
@@ -0,0 +1,22 @@
+// if else Statement
+// An if-else expression usually contains relational operators, which evaluate the expression.
+
+// Equal Operator  ==
+// Not Equal Operator  !=
+// Greater than Operator   >
+// Less than Operator   <
+// Greater than or Equal   >=
+// Less than or Equal   <=
+
+package PracticeFile3;
+public class P24 {
+    public static void main(String[] args) {
+        int x = 25;
+        if (x == 20){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+        // Output - No
+    }
+}

89af3efc423d15cd0dc4318b7fc9d1279dedf43c
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-11 23:57:06 +0530

    final keyword for Method

--- Java/CodingPractice/File 1/src/PracticeFile3/P27.java
+++ Java/CodingPractice/File 1/src/PracticeFile3/P27.java
@@ -3,10 +3,28 @@
 package PracticeFile3;
 
 /************ final for Variable ************/
-public class P27 {
-    public static void main(String[] args) {
-        final double a = 5.75;
-        // a = 55; // Cannot assign a value to final variable 'a'
-        System.out.println(a);
-    }
-}
+//public class P27 {
+//    public static void main(String[] args) {
+//        final double a = 5.75;
+//        // a = 55; // Cannot assign a value to final variable 'a'
+//        System.out.println(a);
+//    }
+//}
+
+/************ final for Method ************/
+//class Parent{
+//    final public void say(){
+//        System.out.println("Hi");
+//    }
+//}
+//class Child extends Parent{
+////    public void say(){  //'say()' cannot override 'say()' in 'Parent'; overridden method is final
+////        System.out.println("Hello");
+////    }
+//}
+//public class P27 {
+//    public static void main(String[] args) {
+//        Child obj = new Child();
+//        obj.say();
+//    }
+//}

8c6d78c0ad1017c110852ff34219f99a6b62b908
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-12 23:59:24 +0530

    Break Statement

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P30.java
@@ -0,0 +1,15 @@
+// Break and Continue
+
+package PracticeFile3;
+
+public class P30 {
+    public static void main(String[] args) {
+
+//        Break Statement
+        for (int i=1; i<=10; i++){
+            if (i==5)
+                break;;
+            System.out.println(i);
+        }
+    }
+}

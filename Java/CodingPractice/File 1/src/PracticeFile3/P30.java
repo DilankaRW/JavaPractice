e7bc3adb6a85b1b69f3c226f5b4feaf61ca50d9b
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-13 00:03:26 +0530

    Continue Statement

--- Java/CodingPractice/File 1/src/PracticeFile3/P30.java
+++ Java/CodingPractice/File 1/src/PracticeFile3/P30.java
@@ -5,11 +5,23 @@ package PracticeFile3;
 public class P30 {
     public static void main(String[] args) {
 
-//        Break Statement
+        /*
+        // Break Statement
         for (int i=1; i<=10; i++){
             if (i==5)
-                break;;
+                break;
             System.out.println(i);
         }
+        // Output - 1,2,3,4
+         */
+
+        // Continue Statement
+        for (int i=1; i<=10; i++){
+            if (i==5)
+                continue;
+            System.out.println(i);
+        }
+        // Output - 1,2,3,4,6,7,8,9,10
+        // Number 5 does not print
     }
 }

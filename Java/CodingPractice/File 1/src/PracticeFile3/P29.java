f3903c9baa4c7a5873301d4a0f8cb1ea82fc9b60
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-12 23:50:08 +0530

    Multiple Inheritance

---
+++ Java/CodingPractice/File 1/src/PracticeFile3/P29.java
@@ -0,0 +1,28 @@
+// Multiple Inheritance
+
+package PracticeFile3;
+
+/*
+class A{
+}
+interface B{
+}
+class C extends A implements B{
+}
+*/
+
+/*
+interface A{
+}
+interface B{
+}
+class C implements A,B{
+}
+*/
+
+interface A{
+}
+interface B{
+}
+interface C extends A,B{
+}

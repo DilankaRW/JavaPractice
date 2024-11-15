b33b683c513906f12bade12378e196bbe15ce550
VL.dilankarw <dilankarashmika10@gmail.com>
2024-11-15 23:47:37 +0530

    String Reverse / Remove Character

--- Java/CodingPractice/File 1/src/PracticeFile3/P32.java
+++ Java/CodingPractice/File 1/src/PracticeFile3/P32.java
@@ -1,3 +1,5 @@
+// String Reverse
+// Remove Character
 // pyramid of characters.
 
 package PracticeFile3;
@@ -6,6 +8,14 @@ import java.util.Scanner;
 
 public class P32 {
     public static void main(String[] args) {
+        String S1 = "hiru";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "hiru";
+        S2 = S2.replaceAll("i","*");
+        System.out.println(S2);
+
         Scanner S = new Scanner(System.in);
         System.out.print("Rows Count : ");
         int row = S.nextInt();

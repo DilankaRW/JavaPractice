2bf8bf461d3c4bf13f936efb357c3d20fc5ccb4f
VL.dilankarw <dilankarashmika10@gmail.com>
2024-12-01 23:19:00 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile2/P31.java
+++ Java/CodingPractice/File 1/src/PracticeFile2/P31.java
@@ -3,6 +3,9 @@
 // Remove leading and trailing spaces from a string.
 // Remove leading spaces from a string
 // Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
 
 package PracticeFile2;
 
@@ -31,5 +34,21 @@ public class P31 {
         S5 = S5.replaceAll("\\s+$","");
         System.out.println(S5);
 
+        String[] S6 = {"s","a","r","o"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        Integer[] S7 = {23,56,22,6,5,12,54};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {34,6,4,23};
+        int sum = 0;
+        for (int i : S8) sum += i;
+        System.out.println(sum);
+
+        Integer[] S9 = {34,56,31,45,12,53};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
     }
 }

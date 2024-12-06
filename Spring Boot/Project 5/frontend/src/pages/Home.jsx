63883d618cafa27aff98745fea0657aa556e1986
VL.dilankarw <dilankarashmika10@gmail.com>
2024-12-06 14:22:43 +0530

    Create Homepage using Bootstrap table

---
+++ Spring Boot/Project 5/frontend/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table className="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">email</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">*</th>
+              <td>Dilanka</td>
+              <td>dilanka</td>
+              <td>dilanka@email.com</td>
+              <td>
+                <button className="btn btn-primary mx-2">View</button>
+                <button className="btn btn-outline-primary mx-2">Edit</button>
+                <button className="btn btn-danger mx-2">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};

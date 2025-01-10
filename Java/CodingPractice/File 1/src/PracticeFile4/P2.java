dcef773cceaa675df5b5ac95021dadc5b1997dbe
VL.dilankarw <dilankarashmika10@gmail.com>
2025-01-10 23:55:29 +0530

    String Reverse / Remove Character

--- Java/CodingPractice/File 1/src/PracticeFile4/P2.java
+++ Java/CodingPractice/File 1/src/PracticeFile4/P2.java
@@ -1,2 +1,16 @@
-package PracticeFile4;public class P2 {
+// String Reverse
+// Remove Character
+
+package PracticeFile4;
+
+public class P2 {
+    public static void main(String[] args) {
+        String S1 = "naro";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "naro";
+        S2 = S2.replaceAll("o","i");
+        System.out.println(S2);
+    }
 }

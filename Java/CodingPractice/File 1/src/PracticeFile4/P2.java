c755adbb97d3e590c0c3b175c48442db069574e0
VL.dilankarw <dilankarashmika10@gmail.com>
2025-01-12 23:53:58 +0530

    sort array, sum array, Descending order Array, Shuffle an array

--- Java/CodingPractice/File 1/src/PracticeFile4/P2.java
+++ Java/CodingPractice/File 1/src/PracticeFile4/P2.java
@@ -3,8 +3,16 @@
 // Remove leading and trailing spaces from a string.
 // Remove leading spaces from a string
 // Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
 
 package PracticeFile4;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class P2 {
     public static void main(String[] args) {
@@ -27,5 +35,28 @@ public class P2 {
         String S5 = "naro   ";
         S5 = S5.replaceAll("\\s+$","");
         System.out.println(S5);
+
+        String[] S6 = {"n","a","r","o"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        int[] S7 = {56,78,32,5};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {34,676,23};
+        int sum = 0;
+        for (int i : S8)sum +=i;
+        System.out.println(sum);
+
+        Integer[] S9 = {23,67,21,45};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
+
+        Integer[] S10 = {23,45,75};
+        List<Integer> list = Arrays.asList(S10);
+        Collections.shuffle(list);
+        list.toArray(S10);
+        System.out.println(Arrays.toString(S10));
     }
 }

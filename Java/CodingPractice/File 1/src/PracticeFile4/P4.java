03da4ffd6d5f871e9b998aad496b6dd43f3cca45
VL.dilankarw <dilankarashmika10@gmail.com>
2025-02-19 23:54:10 +0530

    String Reverse / Remove Character / Space Trim

---
+++ Java/CodingPractice/File 1/src/PracticeFile4/P4.java
@@ -0,0 +1,30 @@
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
+// Create a pyramid of characters.
+
+package PracticeFile4;
+
+import java.sql.SQLOutput;
+
+public class P4 {
+    public static void main(String[] args) {
+        String S1 = "Test";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "Test";
+        S2 = S2.replaceAll("t","*");
+        System.out.println(S2);
+
+        String S3 = "    Test    ";
+        S3 = S3.trim();
+        System.out.println(S3);
+    }
+}

0c6e228bbef6d263e75709f7bbdbb1a9fee7b2eb
VL.dilankarw <dilankarashmika10@gmail.com>
2025-03-25 23:57:56 +0530

    Sum Array / Descending order Array / Shuffle an array

--- Java/CodingPractice/File 1/src/PracticeFile4/P5.java
+++ Java/CodingPractice/File 1/src/PracticeFile4/P5.java
@@ -4,10 +4,15 @@
 // Remove leading spaces from a string
 // Remove trailing spaces from a string
 // Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
 
 package PracticeFile4;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class P5 {
     public static void main(String[] args) {
@@ -38,5 +43,20 @@ public class P5 {
         int[] S7 = {34,54,23};
         Arrays.sort(S7);
         System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {34,65,21};
+        int sum = 0;
+        for (int i : S8) sum +=i;
+        System.out.println(sum);
+
+        Integer[] S9 = {34,56,45,23};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
+
+        Integer[] S10 = {23,64,865,45};
+        List<Integer> list = Arrays.asList(S10);
+        Collections.shuffle(list);
+        list.toArray(S10);
+        System.out.println(Arrays.toString(S10));
     }
 }

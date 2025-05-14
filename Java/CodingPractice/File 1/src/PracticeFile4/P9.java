07a00ef35f046e04df11427a5e2eecd7a932a7de
VL.dilankarw <dilankarashmika10@gmail.com>
2025-05-14 10:45:38 +0530

    Java Practice

---
+++ Java/CodingPractice/File 1/src/PracticeFile4/P9.java
@@ -0,0 +1,47 @@
+// String Reverse
+// Remove Character
+// Remove leading and trailing spaces from a string.
+// Remove leading spaces from a string
+// Remove trailing spaces from a string
+// Sort Array
+// Sum Array
+// Descending order Array
+// Shuffle an array
+// Create a pyramid of characters.
+
+package PracticeFile4;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class P9 {
+    public static void main(String[] args) {
+        String S1 = "Test";
+        S1 = new StringBuilder(S1).reverse().toString();
+        System.out.println(S1);
+
+        String S2 = "Test";
+        S2 = S2.replaceAll("s","S");
+        System.out.println(S2);
+
+        String S3 = "   Test  ";
+        S3 = S3.trim();
+        System.out.println(S3);
+
+        String S4 = "   Test";
+        S4 = S4.replaceAll("^\\s+","");
+        System.out.println(S4);
+
+        String S5 = "Test   ";
+        S5 = S5.replaceAll("\\s+$","");
+        System.out.println(S5);
+
+        String[] S6 = {"T","e","s","t"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        int[] S7 = {34,65,3,4,74};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+    }
+}

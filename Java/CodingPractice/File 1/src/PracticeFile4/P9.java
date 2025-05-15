df0a4137e2dc8a9e5e733b63cfb545e4d5ec2c73
VL.dilankarw <dilankarashmika10@gmail.com>
2025-05-15 10:50:37 +0530

    Practice

--- Java/CodingPractice/File 1/src/PracticeFile4/P9.java
+++ Java/CodingPractice/File 1/src/PracticeFile4/P9.java
@@ -15,6 +15,7 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class P9 {
     public static void main(String[] args) {
@@ -60,5 +61,19 @@ public class P9 {
         Collections.shuffle(list);
         list.toArray(S10);
         System.out.println(Arrays.toString(S10));
+
+        Scanner S = new Scanner(System.in);
+        System.out.print("Rows Count : ");
+        int row = S.nextInt();
+        S.close();
+        for (int i=1; i<=row; i++){
+            for (int x=row-i; x>0; x--){
+                System.out.print(" ");
+            }
+            for (int x=1; x<=(2*i-1); x++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }

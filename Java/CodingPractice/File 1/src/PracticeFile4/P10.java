d14516dcb0886c9279b3b37f742c2ef3e5aefb54
VL.dilankarw <dilankarashmika10@gmail.com>
2025-05-19 11:18:57 +0530

    Java Practice

--- Java/CodingPractice/File 1/src/PracticeFile4/P10.java
+++ Java/CodingPractice/File 1/src/PracticeFile4/P10.java
@@ -3,8 +3,13 @@
 // Remove leading and trailing spaces from a string.
 // Remove leading spaces from a string
 // Remove trailing spaces from a string
+// Sort Array
+// Sum Array
 
 package PracticeFile4;
+
+import java.util.Arrays;
+
 public class P10 {
     public static void main(String[] args) {
         String S1 = "xcot";
@@ -26,5 +31,18 @@ public class P10 {
         String S5 = "xcot    ";
         S5 = S5.replaceAll("\\s+$","");
         System.out.println(S5);
+
+        String[] S6 = {"x","c","o","t"};
+        Arrays.sort(S6);
+        System.out.println(Arrays.toString(S6));
+
+        int[] S7 = {65,3,6,42,23};
+        Arrays.sort(S7);
+        System.out.println(Arrays.toString(S7));
+
+        int[] S8 = {34,23,43};
+        int sum = 0;
+        for (int i : S8) sum +=i;
+        System.out.println(sum);
     }
 }

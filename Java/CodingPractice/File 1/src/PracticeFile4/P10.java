c624ef9b86bb160d11f174e06e7422f83c4b8ac9
VL.dilankarw <dilankarashmika10@gmail.com>
2025-05-19 11:28:08 +0530

    Java Practice

--- Java/CodingPractice/File 1/src/PracticeFile4/P10.java
+++ Java/CodingPractice/File 1/src/PracticeFile4/P10.java
@@ -5,10 +5,16 @@
 // Remove trailing spaces from a string
 // Sort Array
 // Sum Array
+// Descending order Array
+// Shuffle an array
+// Create a pyramid of characters.
 
 package PracticeFile4;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class P10 {
     public static void main(String[] args) {
@@ -44,5 +50,29 @@ public class P10 {
         int sum = 0;
         for (int i : S8) sum +=i;
         System.out.println(sum);
+
+        Integer[] S9 = {32,45,13,67,23};
+        Arrays.sort(S9, Collections.reverseOrder());
+        System.out.println(Arrays.toString(S9));
+
+        Integer[] S10 = {76,45,23,64,31};
+        List<Integer> list = Arrays.asList(S10);
+        Collections.shuffle(list);
+        list.toArray(S10);
+        System.out.println(Arrays.toString(S10));
+
+        Scanner S = new Scanner(System.in);
+        System.out.print("Rows Count : ");
+        int row = S.nextInt();
+        S.close();
+        for (int i=1; i<row; i++){
+            for (int x=row-i; x>0; x--){
+                System.out.print(" ");
+            }
+            for (int x=1; x<=(2*i-1); x++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }

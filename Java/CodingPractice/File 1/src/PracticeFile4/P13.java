726bebbd5158a94f2e1c426c53fc0df5a77ed752
VL.dilankarw <dilankarashmika10@gmail.com>
2025-05-23 12:46:06 +0530

    Swap two numbers

--- Java/CodingPractice/File 1/src/PracticeFile4/P13.java
+++ Java/CodingPractice/File 1/src/PracticeFile4/P13.java
@@ -1,2 +1,18 @@
-package PracticeFile4;public class P13 {
+// Swap two numbers without using a third variable.
+
+package PracticeFile4;
+
+public class P13 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a is " + a + " and b is " + b);
+
+        a = a+b;
+        b = a-b;
+        a = a-b;
+
+        System.out.println("After swapping, a is " + a + " and b is " + b);
+    }
 }

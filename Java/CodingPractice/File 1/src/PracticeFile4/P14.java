ee939a986d4c8e34b8caf9b4c7df9fc484bca07d
VL.dilankarw <dilankarashmika10@gmail.com>
2025-05-25 09:52:12 +0530

    Create text blocks in Java.

---
+++ Java/CodingPractice/File 1/src/PracticeFile4/P14.java
@@ -0,0 +1,15 @@
+// Create text blocks in Java.
+
+package PracticeFile4;
+
+public class P14 {
+    public static void main(String[] args) {
+        String TextBlock = """
+                This is a text block.
+                It can span multiple lines.
+                Each new line in the source code is also a new line in the string.
+                """;
+
+        System.out.println(TextBlock);
+    }
+}
